e917f63ac0bc78f687d16c5ead36902c30f1ab52
Igor Tuag <igortuag@gmail.com>
2021-01-03 08:19:48 -0400

    config size heading

--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -1,3 +1,12 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
-export const Wrapper = styled.h2``
+export const Wrapper = styled.h2`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.xlarge};
+
+    ${media.greaterThan('medium')`
+      font-size: ${theme.font.sizes.xxlarge};
+    `}
+  `}
+`
