c89a7ff7d4ed74c0b75d46aa67ef1206a0d72b11
Igor Tuag <igortuag@gmail.com>
2021-01-03 08:22:28 -0400

    config heading colors

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -2,10 +2,11 @@ import * as S from './styles'
 
 export type HeadingProps = {
   children: React.ReactNode
+  color: 'white' | 'black'
 }
 
-const Heading = ({ children }: HeadingProps) => (
-  <S.Wrapper>{children}</S.Wrapper>
+const Heading = ({ children, color = 'white' }: HeadingProps) => (
+  <S.Wrapper color={color}>{children}</S.Wrapper>
 )
 
 export default Heading
--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -1,8 +1,10 @@
 import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
+import { HeadingProps } from '.'
 
-export const Wrapper = styled.h2`
-  ${({ theme }) => css`
+export const Wrapper = styled.h2<HeadingProps>`
+  ${({ theme, color }) => css`
+    color: ${theme.colors[color]};
     font-size: ${theme.font.sizes.xlarge};
 
     ${media.greaterThan('medium')`
