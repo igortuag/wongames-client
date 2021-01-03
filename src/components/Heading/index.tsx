122d5323be775a94af372086e1893c5c07a79434
Igor Tuag <igortuag@gmail.com>
2021-01-03 08:29:29 -0400

    config line left

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -3,10 +3,17 @@ import * as S from './styles'
 export type HeadingProps = {
   children: React.ReactNode
   color: 'white' | 'black'
+  lineLeft?: boolean
 }
 
-const Heading = ({ children, color = 'white' }: HeadingProps) => (
-  <S.Wrapper color={color}>{children}</S.Wrapper>
+const Heading = ({
+  children,
+  color = 'white',
+  lineLeft = false
+}: HeadingProps) => (
+  <S.Wrapper color={color} lineLeft={lineLeft}>
+    {children}
+  </S.Wrapper>
 )
 
 export default Heading
--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -1,14 +1,23 @@
-import styled, { css } from 'styled-components'
+import styled, { css, DefaultTheme } from 'styled-components'
 import media from 'styled-media-query'
 import { HeadingProps } from '.'
 
+const wrapperModifiers = {
+  lineLeft: (theme: DefaultTheme) => css`
+    padding-left: ${theme.spacings.xxsmall};
+    border-left: 0.7rem solid ${theme.colors.secondary};
+  `
+}
+
 export const Wrapper = styled.h2<HeadingProps>`
-  ${({ theme, color }) => css`
+  ${({ theme, color, lineLeft }) => css`
     color: ${theme.colors[color]};
     font-size: ${theme.font.sizes.xlarge};
 
     ${media.greaterThan('medium')`
       font-size: ${theme.font.sizes.xxlarge};
     `}
+
+    ${lineLeft && wrapperModifiers.lineLeft(theme)}
   `}
 `
