bccb189622f343f155fc2652b09421b2cd2cb72a
Igor Tuag <igortuag@gmail.com>
2021-01-03 16:51:49 -0400

    test to expect a heding with a line to the left side

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -17,4 +17,12 @@ describe('<Heading />', () => {
       color: '#030517'
     })
   })
+
+  it('should render a heading with a line to the left side', () => {
+    renderWithTheme(<Heading lineLeft>Won Games</Heading>)
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
+      'border-left': '0.7rem solid #3CD3C1'
+    })
+  })
+
 })
