98fb91c192a1bda9067e68a2c77f1ea08d037b23
Igor Tuag <igortuag@gmail.com>
2021-01-05 08:24:05 -0400

    add background gradient to button

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,6 +1,10 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 import { ButtonProps } from '.'
 
 type WrapperProps = Pick<ButtonProps, 'size'>
 
-export const Wrapper = styled.button<WrapperProps>``
+export const Wrapper = styled.button<WrapperProps>`
+  ${({ theme, size }) => css`
+    background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
+  `}
+`
