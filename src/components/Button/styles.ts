c97e67cafd649491e094ac03d0660a4cb14dad4b
Igor Tuag <igortuag@gmail.com>
2021-01-05 09:03:10 -0400

    config diferents sizes

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,7 +1,24 @@
-import styled, { css } from 'styled-components'
+import styled, { css, DefaultTheme } from 'styled-components'
 import { ButtonProps } from '.'
 
 type WrapperProps = Pick<ButtonProps, 'size'>
+
+const wrapperModifiers = {
+  small: (theme: DefaultTheme) => css`
+    height: 3rem;
+    font-size: ${theme.font.sizes.xsmall};
+  `,
+  medium: (theme: DefaultTheme) => css`
+    height: 4rem;
+    font-size: ${theme.font.sizes.small};
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.medium};
+  `,
+  large: (theme: DefaultTheme) => css`
+    height: 5rem;
+    font-size: ${theme.font.sizes.medium};
+    padding: ${theme.spacings.xxsmall} ${theme.spacings.xlarge};
+  `
+}
 
 export const Wrapper = styled.button<WrapperProps>`
   ${({ theme, size }) => css`
@@ -10,5 +27,7 @@ export const Wrapper = styled.button<WrapperProps>`
     border: 0;
     border-radius: ${theme.border.radius};
     padding: ${theme.spacings.xxsmall};
+
+    ${!!size && wrapperModifiers[size](theme)}
   `}
 `
