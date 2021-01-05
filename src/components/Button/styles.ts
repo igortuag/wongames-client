66f757e8413addf220216580cea319bc308e73c0
Igor Tuag <igortuag@gmail.com>
2021-01-05 08:26:55 -0400

    add default styles

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -6,5 +6,9 @@ type WrapperProps = Pick<ButtonProps, 'size'>
 export const Wrapper = styled.button<WrapperProps>`
   ${({ theme, size }) => css`
     background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
+    color: ${theme.colors.white};
+    border: 0;
+    border-radius: ${theme.border.radius};
+    padding: ${theme.spacings.xxsmall};
   `}
 `
