0d2d24b1c5c7c9e57ac9d0a20783ca3f84a5477a
Igor Tuag <igortuag@gmail.com>
2021-01-06 07:41:20 -0400

    test button medium size buy default

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -1,13 +1,16 @@
 import { render, screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helper'
 
 import Button from '.'
 
 describe('<Button />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Button />)
+  it('should render a medium size by default', () => {
+    renderWithTheme(<Button>Buy Now</Button>)
 
-    expect(screen.getByRole('heading', { name: /Button/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
+      height: '4rem',
+      padding: '0.8rem 3.2rem',
+      'font-size': '1.4rem'
+    })
   })
 })
