203d52c17b87f8b9fac8db823db93d06e817e9b9
Igor Tuag <igortuag@gmail.com>
2021-01-07 08:50:53 -0400

    add and config snapshot

--- src/components/Button/test.tsx
+++ src/components/Button/test.tsx
@@ -6,13 +6,15 @@ import Button from '.'
 
 describe('<Button />', () => {
   it('should render a medium size by default', () => {
-    renderWithTheme(<Button>Buy Now</Button>)
+    const { container } = renderWithTheme(<Button>Buy Now</Button>)
 
     expect(screen.getByRole('button', { name: /Buy Now/i })).toHaveStyle({
       height: '4rem',
       padding: '0.8rem 3.2rem',
       'font-size': '1.4rem'
     })
+
+    expect(container.firstChild).toMatchSnapshot()
   })
 
   it('should render a small size', () => {
