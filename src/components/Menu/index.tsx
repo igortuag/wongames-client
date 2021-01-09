dda840f38acd99cb566a4800b190fa13989174d3
Igor Tuag <igortuag@gmail.com>
2021-01-09 09:30:15 -0400

    close menu on click code

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 
 import Logo from 'components/Logo'
 import * as S from './styles'
@@ -26,7 +27,9 @@ const Menu = () => {
         </S.IconWrapper>
       </S.MenuGroup>
 
-      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}></S.MenuFull>
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+      </S.MenuFull>
     </S.Wrapper>
   )
 }
