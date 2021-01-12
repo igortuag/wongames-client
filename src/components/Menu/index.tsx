6e236ec576a43cb2185f3354e5fad56c2b5b0061
Igor Tuag <igortuag@gmail.com>
2021-01-12 08:16:14 -0400

    show user links only if has username

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -8,7 +8,11 @@ import Logo from 'components/Logo'
 import * as S from './styles'
 import Button from 'components/Button'
 
-const Menu = () => {
+export type MenuProps = {
+  username?: string
+}
+
+const Menu = ({ username }: MenuProps) => {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
@@ -34,10 +38,12 @@ const Menu = () => {
           <S.MenuLink href="#">Home</S.MenuLink>
           <S.MenuLink href="#">Explore</S.MenuLink>
 
-          <>
-            <S.MenuLink href="#">My account</S.MenuLink>
-            <S.MenuLink href="#">Wishlist</S.MenuLink>
-          </>
+          {!!username && (
+            <>
+              <S.MenuLink href="#">My account</S.MenuLink>
+              <S.MenuLink href="#">Wishlist</S.MenuLink>
+            </>
+          )}
         </S.MenuNav>
 
         <S.RegisterBox>
