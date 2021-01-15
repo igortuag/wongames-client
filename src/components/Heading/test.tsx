01902877c6cd2a3c171735c956abafceabad8012
Igor Tuag <igortuag@gmail.com>
2021-01-15 07:53:40 -0400

    test to check line after width

--- src/components/Heading/test.tsx
+++ src/components/Heading/test.tsx
@@ -41,5 +41,13 @@ describe('<Heading />', () => {
     expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyle({
       'font-size': '1.6rem'
     })
+
+    expect(screen.getByRole('heading', { name: /won games/i })).toHaveStyleRule(
+      'width',
+      '3rem',
+      {
+        modifier: '::after'
+      }
+    )
   })
 })
