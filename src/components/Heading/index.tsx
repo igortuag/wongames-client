33d3bf5b269be7146a36eae77bdef6fc86ebf1e1
Igor Tuag <igortuag@gmail.com>
2021-01-15 08:03:07 -0400

    code to change heading color

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,10 +1,13 @@
 import * as S from './styles'
 
+export type LineColorProps = 'primary' | 'secondary'
+
 export type HeadingProps = {
   children: React.ReactNode
   color?: 'white' | 'black'
   lineLeft?: boolean
   lineBottom?: boolean
+  lineColor?: LineColorProps
   size?: 'small' | 'medium'
 }
 
@@ -13,12 +16,14 @@ const Heading = ({
   color = 'white',
   lineLeft = false,
   lineBottom = false,
+  lineColor = 'primary',
   size = 'medium'
 }: HeadingProps) => (
   <S.Wrapper
     color={color}
     lineLeft={lineLeft}
     lineBottom={lineBottom}
+    lineColor={lineColor}
     size={size}
   >
     {children}
