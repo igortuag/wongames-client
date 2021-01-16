d18d2ebe5edda4fae9cfa20b56de9e6af0e30cd3
Igor Tuag <igortuag@gmail.com>
2021-01-16 14:40:42 -0400

    add contact on footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,20 @@
 import Logo from 'components/Logo'
+import Heading from 'components/Heading'
 
 import * as S from './styles'
 
 const Footer = () => (
   <S.Wrapper>
     <Logo color="black" />
+    <S.Content>
+      <S.Column>
+        <Heading color="black" size="small" lineBottom lineColor="secondary">
+          Contact
+        </Heading>
+
+        <a href="mailto:sac@wongames.com">sac@wongames.com</a>
+      </S.Column>
+    </S.Content>
   </S.Wrapper>
 )
 
