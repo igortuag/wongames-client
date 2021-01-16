65b5d3a35af9f7929ac2e01cdfeea8f27aff8bf5
Igor Tuag <igortuag@gmail.com>
2021-01-16 14:52:03 -0400

    add content styles footer

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import * as HeadingStyles from 'components/Heading/styles'
 
@@ -8,7 +8,14 @@ export const Wrapper = styled.footer`
   }
 `
 
-export const Content = styled.div``
+export const Content = styled.div`
+  ${({ theme }) => css`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: ${theme.grid.gutter};
+    margin-top: ${theme.spacings.medium};
+  `}
+`
 
 export const Column = styled.div``
 
