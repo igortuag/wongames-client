69894768b26cfb91bdba2da696ed00d5fcfd7b10
Igor Tuag <igortuag@gmail.com>
2021-01-16 14:39:17 -0400

    footer structure

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,3 +1,9 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.footer``
+
+export const Content = styled.div``
+
+export const Column = styled.div``
+
+export const Copyright = styled.div``
