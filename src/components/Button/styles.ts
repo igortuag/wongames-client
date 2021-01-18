30a68af975bdc41d23db28ed5cbc20f0f057e0d1
Igor Tuag <igortuag@gmail.com>
2021-01-18 08:10:50 -0300

    change default display css

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -24,10 +24,6 @@ const wrapperModifiers = {
     width: 100%;
   `,
   withIcon: (theme: DefaultTheme) => css`
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-
     svg {
       width: 1.5rem;
 
@@ -40,6 +36,9 @@ const wrapperModifiers = {
 
 export const Wrapper = styled.button<WrapperProps>`
   ${({ theme, size, fullWidth, hasIcon }) => css`
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
     background: linear-gradient(180deg, #ff5f5f 0%, #f062c0 50%);
     color: ${theme.colors.white};
     border: 0;
