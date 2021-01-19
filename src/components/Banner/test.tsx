e0791436edd1298a4f51f3303c8eca4d54ae3db2
Igor Tuag <igortuag@gmail.com>
2021-01-19 08:05:58 -0300

    test questions

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -3,11 +3,9 @@ import { render, screen } from '@testing-library/react'
 import Banner from '.'
 
 describe('<Banner />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Banner />)
-
-    expect(screen.getByRole('heading', { name: /Banner/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+  it('should render correctly', () => {
+    // verifique se o title existe renderizado (.toBeInTheDocument())
+    // verifique se o subtitle existe renderizado
+    // verifique se a imagem existe renderizado
   })
 })
