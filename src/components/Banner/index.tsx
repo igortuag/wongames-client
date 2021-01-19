6c76b7b4d913fc6d3e5beeb1f6f28d398d9e6ce7
Igor Tuag <igortuag@gmail.com>
2021-01-19 07:54:31 -0300

    define banner props

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,5 +1,13 @@
 import * as S from './styles'
 
+export type BannerProps = {
+  img: string
+  title: string
+  subtitle: string
+  buttonLabel: string
+  buttonLink: string
+}
+
 const Banner = () => (
   <S.Wrapper>
     <h1>Banner</h1>
