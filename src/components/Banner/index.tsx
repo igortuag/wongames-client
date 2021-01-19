2396fdbd24ed1eb92cdf3b9b47b6d9ab0c902526
Igor Tuag <igortuag@gmail.com>
2021-01-19 07:51:29 -0300

    Generate banner

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+const Banner = () => (
+  <S.Wrapper>
+    <h1>Banner</h1>
+  </S.Wrapper>
+)
+
+export default Banner
---
+++ src/components/Banner/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Banner from '.'
+
+export default {
+  title: 'Banner',
+  component: Banner
+} as Meta
+
+export const Default: Story = () => <Banner />
