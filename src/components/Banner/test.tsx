dfa6dc70a042baec6865b227f258f830580142d7
Igor Tuag <igortuag@gmail.com>
2021-01-20 08:05:20 -0300

    test if title to be in the document

--- src/components/Banner/test.tsx
+++ src/components/Banner/test.tsx
@@ -1,11 +1,19 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helper'
 
 import Banner from '.'
 
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+  title: 'Defy death',
+  subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+  buttonLabel: 'Buy now',
+  buttonLink: '/games/defy-death'
+}
+
 describe('<Banner />', () => {
   it('should render correctly', () => {
-    // verifique se o title existe renderizado (.toBeInTheDocument())
-    // verifique se o subtitle existe renderizado
-    // verifique se a imagem existe renderizado
+    renderWithTheme(<Banner {...props} />)
+    expect(screen.getByRole('heading', { name: /Defy death/i }))
   })
 })
