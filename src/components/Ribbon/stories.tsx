6bffe3cc9865fb1a4b3bc09404427034e6076a27
Igor Tuag <igortuag@gmail.com>
2021-01-22 08:35:50 -0300

    config storybook to component ribbon

--- src/components/Ribbon/stories.tsx
+++ src/components/Ribbon/stories.tsx
@@ -1,12 +1,20 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import Ribbon from '.'
+import Ribbon, { RibbonProps } from '.'
 
 export default {
   title: 'Ribbon',
-  component: Ribbon
+  component: Ribbon,
+  args: {
+    children: 'Best Seller'
+  },
+  argTypes: {
+    children: {
+      type: 'string'
+    }
+  }
 } as Meta
 
-export const Default: Story = () => (
+export const Default: Story<RibbonProps> = (args) => (
   <div
     style={{
       width: '40rem',
@@ -15,6 +23,6 @@ export const Default: Story = () => (
       background: '#888'
     }}
   >
-    <Ribbon />
+    <Ribbon {...args} />
   </div>
 )
