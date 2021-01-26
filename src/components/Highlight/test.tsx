40d9897a174f762a16efb2457c4cecceb95e47eb
Igor Tuag <igortuag@gmail.com>
2021-01-26 07:30:41 -0300

    generate component highlight

---
+++ src/components/Highlight/test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react'
+
+import Highlight from '.'
+
+describe('<Highlight />', () => {
+  it('should render the heading', () => {
+    const { container } = render(<Highlight />)
+
+    expect(screen.getByRole('heading', { name: /Highlight/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
