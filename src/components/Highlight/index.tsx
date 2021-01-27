3f5911dd280c02f2308dfc0b6c6c3d40a1b89aa1
Igor Tuag <igortuag@gmail.com>
2021-01-27 07:42:51 -0300

    should render headings and button code

--- src/components/Highlight/index.tsx
+++ src/components/Highlight/index.tsx
@@ -1,8 +1,14 @@
 import * as S from './styles'
 
-const Highlight = () => (
+export type HighlightProps = {
+  title: string
+  subtitle: string
+}
+
+const Highlight = ({ title, subtitle }: HighlightProps) => (
   <S.Wrapper>
-    <h1>Highlight</h1>
+    <S.Title>{title}</S.Title>
+    <S.Subtitle>{subtitle}</S.Subtitle>
   </S.Wrapper>
 )
 
