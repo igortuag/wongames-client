cf1d9baf50c0a2aac16630199ae17e8373c749a5
Igor Tuag <igortuag@gmail.com>
2021-01-28 07:38:15 -0300

    Wrapper highlight style

--- src/components/Highlight/styles.ts
+++ src/components/Highlight/styles.ts
@@ -1,6 +1,20 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.main`
+  ${({ theme }) => css`
+    position: relative;
+    height: 23rem;
+    display: grid;
+
+    &:after {
+      content: '';
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, -0.6);
+    }
+  `}
+`
 
 export const Title = styled.h2``
 
