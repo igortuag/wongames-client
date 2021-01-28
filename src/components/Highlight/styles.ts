523e69d1e44f0b1b2e566f53ac06b458e16a2bcf
Igor Tuag <igortuag@gmail.com>
2021-01-28 07:49:42 -0300

    should render a background image test

--- src/components/Highlight/styles.ts
+++ src/components/Highlight/styles.ts
@@ -1,6 +1,6 @@
 import styled, { css } from 'styled-components'
 
-export const Wrapper = styled.main`
+export const Wrapper = styled.section`
   ${({ theme }) => css`
     position: relative;
     height: 23rem;
