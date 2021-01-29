551dfcfe5e57bc595a40cac61f1debc579178804
Igor Tuag <igortuag@gmail.com>
2021-01-29 07:46:53 -0300

    config stories book to add float image

--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -2,15 +2,25 @@ import { Story, Meta } from '@storybook/react/types-6-0'
 import Highlight, { HighlightProps } from '.'
 
 export default {
-  title: 'Highlight',
-  component: Highlight,
-  args: {
-    title: "Read Dead it's back",
-    subtitle: "Come see John's new adventures",
-    backgroundImage: 'img/red-dead-img.png',
-    buttonLabel: 'Buy now',
-    buttonLink: '/rdr2'
-  }
+  title: 'Read Dead is back!',
+  subtitle: 'Come see John’s new adventures',
+  backgroundImage: '/img/red-dead-img.jpg',
+  buttonLabel: 'Buy now',
+  buttonLink: '/rdr2'
 } as Meta
 
-export const Default: Story<HighlightProps> = (args) => <Highlight {...args} />
+export const Default: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+export const WithFloatImage: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+WithFloatImage.args = {
+  floatImage: '/img/red-dead-float.png'
+}
