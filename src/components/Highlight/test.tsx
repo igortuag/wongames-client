d7a449ace60b4c0078e1f48146aea8a37a1c2dca
Igor Tuag <igortuag@gmail.com>
2021-01-29 07:42:13 -0300

    should render a float image test

--- src/components/Highlight/test.tsx
+++ src/components/Highlight/test.tsx
@@ -33,4 +33,13 @@ describe('<Highlight />', () => {
       backgroundImage: `url(${props.backgroundImage})`
     })
   })
+
+  it('should render a float image', () => {
+    renderWithTheme(<Highlight {...props} floatImage="/float-image.png" />)
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      '/float-image.png'
+    )
+  })
 })
