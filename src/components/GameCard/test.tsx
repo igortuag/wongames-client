61cb4983abdea2de549650086e8f1be70e82e04f
Igor Tuag <igortuag@gmail.com>
2021-02-02 07:09:22 -0300

    game card tests

--- src/components/GameCard/test.tsx
+++ src/components/GameCard/test.tsx
@@ -1,13 +1,32 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import GameCard from '.'
 
+const props = {
+  title: 'Population Zero',
+  developer: 'Rockstar Games',
+  img: 'https://source.unsplash.com/user/willianjusten/300x140',
+  price: 'R$ 235,00'
+}
+
 describe('<GameCard />', () => {
   it('should render correctly', () => {
-    // renderizar o gameCard
-    // verifiquem se o title foi renderizado
-    // verifiquem se o developer foi renderizado
-    // verifiquem se o img foi renderizado
-    // verifiquem se o price foi renderizado
+    renderWithTheme(<GameCard {...props} />)
+
+    expect(
+      screen.getByRole('heading', { name: props.title })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: props.developer })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      props.img
+    )
+
+    expect(screen.getByLabelText(/add to wishlist/i)).toBeInTheDocument()
   })
 })
