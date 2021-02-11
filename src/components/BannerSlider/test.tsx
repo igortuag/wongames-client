d3c53e41b9ab293931d5851c062b3b118dab78b1
Igor Tuag <igortuag@gmail.com>
2021-02-11 08:07:31 -0300

    should render with 1 active item test

--- src/components/BannerSlider/test.tsx
+++ src/components/BannerSlider/test.tsx
@@ -1,4 +1,6 @@
 import 'match-media-mock'
+import { screen } from '@testing-library/react'
+
 import { renderWithTheme } from 'utils/tests/helper'
 
 import BannerSlider from '.'
@@ -26,5 +28,20 @@ describe('<BannerSlider />', () => {
     const { container } = renderWithTheme(<BannerSlider items={items} />)
 
     expect(container.querySelector('.slick-vertical')).toBeInTheDocument()
+  })
+
+  it('should render with 1 active item', () => {
+    const { container } = renderWithTheme(<BannerSlider items={items} />)
+
+    expect(container.querySelectorAll('.slick-slide')).toHaveLength(2)
+    expect(container.querySelectorAll('li.slick-active')).toHaveLength(1)
+
+    expect(
+      screen.getByRole('heading', { name: /defy death 1/i, hidden: false })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /defy death 2/i, hidden: true })
+    ).toBeInTheDocument()
   })
 })
