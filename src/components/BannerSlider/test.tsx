9a20cf4b613d50d0e6e357f18ffa666d7b1f9a56
Igor Tuag <igortuag@gmail.com>
2021-02-11 07:57:25 -0300

    add items on test BannerSlider

--- src/components/BannerSlider/test.tsx
+++ src/components/BannerSlider/test.tsx
@@ -2,12 +2,30 @@ import { render, screen } from '@testing-library/react'
 
 import BannerSlider from '.'
 
+const items = [
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x580',
+    title: 'Defy death 1',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death',
+    ribbon: 'Bestselling'
+  },
+  {
+    img: 'https://source.unsplash.com/user/willianjusten/1042x582',
+    title: 'Defy death 2',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  }
+]
+
 describe('<BannerSlider />', () => {
   it('should render the heading', () => {
     const { container } = render(<BannerSlider />)
 
-    expect(screen.getByRole('heading', { name: /BannerSlider/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByRole('heading', { name: /BannerSlider/i })
+    ).toBeInTheDocument()
   })
 })
