abb36e9cdf8e5eef5f040bec3675c05c00f0a1e2
Igor Tuag <igortuag@gmail.com>
2021-02-14 08:17:22 -0300

    initial structure gamecardslider add gamecard and slider component

--- src/components/GameCardSlider/index.tsx
+++ src/components/GameCardSlider/index.tsx
@@ -1,8 +1,20 @@
+import GameCard, { GameCardProps } from 'components/GameCard'
+import Slider, { SliderSettings } from 'components/Slider'
 import * as S from './styles'
 
-const GameCardSlider = () => (
+type GameCardSliderProps = {
+  items: GameCardProps[]
+}
+
+const settings: SliderSettings = {}
+
+const GameCardSlider = ({ items }: GameCardSliderProps) => (
   <S.Wrapper>
-    <h1>GameCardSlider</h1>
+    <Slider settings={settings}>
+      {items.map((item, index) => (
+        <GameCard key={index} {...item} />
+      ))}
+    </Slider>
   </S.Wrapper>
 )
 
