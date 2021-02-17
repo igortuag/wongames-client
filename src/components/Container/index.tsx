c0a444340cafb1754721e1883df54ae9b5ebfe68
Igor Tuag <igortuag@gmail.com>
2021-02-17 08:07:45 -0300

    generate container component

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+const Container = () => (
+  <S.Wrapper>
+    <h1>Container</h1>
+  </S.Wrapper>
+)
+
+export default Container
---
+++ src/components/Container/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import Container from '.'
+
+export default {
+  title: 'Container',
+  component: Container
+} as Meta
+
+export const Default: Story = () => <Container />
