225ce74e427799ad3f248fc2e0bda2e12e8c6f33
Igor Tuag <igortuag@gmail.com>
2021-02-17 08:20:04 -0300

    define styles Container

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,7 +1,11 @@
 import styled, { css } from 'styled-components'
 
 export const Container = styled.div`
-  ${({ theme }) => `
-
+  ${({ theme }) => css`
+    max-width: ${theme.grid.container};
+    margin-left: auto;
+    margin-right: auto;
+    padding-left: calc(${theme.grid.gutter} / 2);
+    padding-right: calc(${theme.grid.gutter} / 2);
   `}
 `
