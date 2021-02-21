5b95eb0d80b5b99b891fc4ebf247f68bc896760e
Igor Tuag <igortuag@gmail.com>
2021-02-21 17:42:42 -0300

    add Heading news on home page

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,5 +1,6 @@
 import { Container } from 'components/Container'
 import Footer from 'components/Footer'
+import Heading from 'components/Heading'
 import Menu from 'components/Menu'
 import * as S from './styles'
 
@@ -7,6 +8,12 @@ const Home = () => (
   <section>
     <Container>
       <Menu />
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary" color="black">
+        News
+      </Heading>
     </Container>
 
     <Container>
