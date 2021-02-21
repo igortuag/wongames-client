14ae666a5c7c9bca2460a0ebe007093c8c24e649
Igor Tuag <igortuag@gmail.com>
2021-02-21 17:45:54 -0300

    add heading Upcomming and Free Games

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -23,6 +23,18 @@ const Home = () => (
     </Container>
 
     <Container>
+      <Heading lineLeft lineColor="secondary">
+        Upcomming
+      </Heading>
+    </Container>
+
+    <Container>
+      <Heading lineLeft lineColor="secondary">
+        Free Games
+      </Heading>
+    </Container>
+
+    <Container>
       <Footer />
     </Container>
   </section>
