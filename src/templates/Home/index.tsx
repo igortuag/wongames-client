cceecd5a2638d17033d0f99e60d0d311d1faa9bd
Igor Tuag <igortuag@gmail.com>
2021-02-23 07:23:35 -0300

    Define HomeTemplateProps

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,10 +1,37 @@
+import { BannerProps } from 'components/Banner'
+import { GameCardProps } from 'components/GameCard'
+import { HighlightProps } from 'components/Highlight'
+
 import { Container } from 'components/Container'
 import Footer from 'components/Footer'
 import Heading from 'components/Heading'
 import Menu from 'components/Menu'
+
 import * as S from './styles'
 
-const Home = ({ heading }: any) => (
+export type HomeTemplateProps = {
+  banners: BannerProps[]
+  newGames: GameCardProps[]
+  mostPopularHighlight: HighlightProps
+  mostPopularGames: GameCardProps[]
+  upcommingGames: GameCardProps[]
+  upcommingHighlight: HighlightProps
+  upcommingMoreGames: GameCardProps[]
+  freeGames: GameCardProps[]
+  freeHighlight: HighlightProps
+}
+
+const Home = ({
+  banners,
+  newGames,
+  mostPopularHighlight,
+  mostPopularGames,
+  upcommingGames,
+  upcommingHighlight,
+  upcommingMoreGames,
+  freeGames,
+  freeHighlight
+}: HomeTemplateProps) => (
   <section>
     <Container>
       <Menu />
