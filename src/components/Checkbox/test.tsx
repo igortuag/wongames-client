97944e6e2760bdc0604ebbf26dae5be3d15eb5b4
Igor Tuag <igortuag@gmail.com>
2021-03-01 07:33:49 -0300

    generate checkbox

---
+++ src/components/Checkbox/test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react'
+
+import Checkbox from '.'
+
+describe('<Checkbox />', () => {
+  it('should render the heading', () => {
+    const { container } = render(<Checkbox />)
+
+    expect(screen.getByRole('heading', { name: /Checkbox/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
