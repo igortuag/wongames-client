bd76627759b7e0b09eb89ff886084250b0b4efed
Igor Tuag <igortuag@gmail.com>
2021-03-02 07:57:39 -0300

    define label and labelFor as optional

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,14 +1,14 @@
 import * as S from './styles'
 
 export type CheckboxProps = {
-  label: string
-  labelFor: string
+  label?: string
+  labelFor?: string
 }
 
-const Checkbox = ({ label, labelFor }: CheckboxProps) => (
+const Checkbox = ({ label, labelFor = '' }: CheckboxProps) => (
   <S.Wrapper>
     <input id={labelFor} type="checkbox" />
-    <label htmlFor={labelFor}>{label}</label>
+    {!!label && <label htmlFor={labelFor}>{label}</label>}
   </S.Wrapper>
 )
 
