a9be1bca194a7dbb8af2bb7721d3d862d75385f2
Igor Tuag <igortuag@gmail.com>
2021-03-09 07:56:17 -0300

    should render with label (white) test

--- src/components/Radio/test.tsx
+++ src/components/Radio/test.tsx
@@ -1,10 +1,19 @@
-import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { renderWithTheme } from 'utils/tests/helper'
 
 import Radio from '.'
 
 describe('<Radio />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Radio />)
+  it('should render with label (white)', () => {
+    const { container } = renderWithTheme(
+      <Radio label="Radio" labelFor="check" value="anyValue" />
+    )
+
+    const label = screen.getByText('Radio')
+    expect(label).toBeInTheDocument()
+    expect(label).toHaveStyle({ color: theme.colors.white })
+    expect(container.firstChild).toMatchSnapshot()
+  })
 
     expect(screen.getByRole('heading', { name: /Radio/i })).toBeInTheDocument()
 
