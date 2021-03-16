eacb60a8c4705fb686e9bb7d8de9dcdb8ccd5293
Igor Tuag <igortuag@gmail.com>
2021-03-16 07:17:27 -0300

    remove auth stories

--- src/templates/Auth/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import Auth from '.'
-
-export default {
-  title: 'Auth',
-  component: Auth
-} as Meta
-
-export const Default: Story = () => <Auth />
