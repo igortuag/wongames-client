a51b4fd860de4c580cf3f7967ea7afd73d5ca7ad
Igor Tuag <igortuag@gmail.com>
2021-03-19 08:04:06 -0300

    add styles to BannerBlock

--- src/templates/Auth/styles.ts
+++ src/templates/Auth/styles.ts
@@ -9,7 +9,27 @@ export const Wrapper = styled.main`
   height: 100vh;
 `
 
-export const BannerBlock = styled.div``
+export const BannerBlock = styled.div`
+  ${({ theme }) => css`
+    position: relative;
+    background-image: url(/img/auth-bg.jpg);
+    background-size: cover;
+    background-position: center center;
+    padding: ${theme.spacings.xxlarge} ${theme.spacings.xxlarge}
+      ${theme.spacings.large};
+
+    &:after {
+      content: '';
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      background: ${theme.colors.black};
+      opacity: 0.85;
+    }
+  `}
+`
 
 export const Subtitle = styled.h3``
 
