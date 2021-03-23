1480e4d40b801dde4ae35131a5a271bfe19bf01e
Igor Tuag <igortuag@gmail.com>
2021-03-23 07:54:09 -0300

    config structure of formSignIn

--- src/components/FormSignIn/stories.tsx
+++ src/components/FormSignIn/stories.tsx
@@ -2,8 +2,12 @@ import { Story, Meta } from '@storybook/react/types-6-0'
 import FormSignIn from '.'
 
 export default {
-  title: 'FormSignIn',
+  title: 'Form/FormSignIn',
   component: FormSignIn
 } as Meta
 
-export const Default: Story = () => <FormSignIn />
+export const Default: Story = () => (
+  <div style={{ width: 300, margin: 'auto' }}>
+    <FormSignIn />
+  </div>
+)
