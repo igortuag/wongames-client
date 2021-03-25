514c2c436cbd9c65e1db022789ce20b87ddb0060
Igor Tuag <igortuag@gmail.com>
2021-03-25 06:51:47 -0300

    add styles to FormLink

--- src/components/FormSignIn/styles.ts
+++ src/components/FormSignIn/styles.ts
@@ -14,6 +14,22 @@ export const Wrapper = styled.div`
     }
   `}
 `
-export const ForgetPassword = styled.a``
+export const FormLink = styled.div`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.small};
+    color: ${theme.colors.black};
+    text-align: center;
+    a {
+      color: ${theme.colors.secondary};
+      text-decoration: none;
+      border-bottom: 0.1rem solid ${theme.colors.secondary};
+      transition: color, border, ${theme.transition.fast};
+      &:hover {
+        color: ${darken(0.1, theme.colors.secondary)};
+        border-bottom: 0.1rem solid ${darken(0.1, theme.colors.secondary)};
+      }
+    }
+  `}
+`
 
-export const FormLink = styled.div``
+export const ForgetPassword = styled.a``
