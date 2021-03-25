60a4337f7e84bfb49f355622f9f54a544cd51fbd
Igor Tuag <igortuag@gmail.com>
2021-03-25 06:51:09 -0300

    add stylers to Wrapper and add imports

--- src/components/FormSignIn/styles.ts
+++ src/components/FormSignIn/styles.ts
@@ -1,7 +1,19 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import { lighten, darken } from 'polished'
 
-export const Wrapper = styled.main``
+import * as TextFieldStyles from 'components/TextField/styles'
+import * as ButtonStyles from 'components/Button/styles'
 
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    ${TextFieldStyles.Wrapper} {
+      margin: ${theme.spacings.xxsmall} 0;
+    }
+    ${ButtonStyles.Wrapper} {
+      margin: ${theme.spacings.medium} auto ${theme.spacings.xsmall};
+    }
+  `}
+`
 export const ForgetPassword = styled.a``
 
 export const FormLink = styled.div``
