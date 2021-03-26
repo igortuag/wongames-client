baf16beb701ec4da1ade0751b84187958ed5fc31
Igor Tuag <igortuag@gmail.com>
2021-03-26 06:47:34 -0300

    should render the form test

--- src/components/FormSignUp/test.tsx
+++ src/components/FormSignUp/test.tsx
@@ -1,12 +1,19 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helper'
 
 import FormSignUp from '.'
 
 describe('<FormSignUp />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<FormSignUp />)
+  it('should render the form', () => {
+    const { container } = renderWithTheme(<FormSignUp />)
 
-    expect(screen.getByRole('heading', { name: /FormSignUp/i })).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/name/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
+    expect(screen.getByPlaceholderText('Password')).toBeInTheDocument()
+    expect(screen.getByPlaceholderText('Confirm password')).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: /sign up now/i })
+    ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
   })
