cbdb197580a71fff0614cc53b573c532cc0a2e9c
Igor Tuag <igortuag@gmail.com>
2021-03-27 14:50:14 -0300

    add link to logos on auth template

--- src/templates/Auth/index.tsx
+++ src/templates/Auth/index.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 import Heading from 'components/Heading'
 import Logo from 'components/Logo'
 import * as S from './styles'
@@ -11,7 +13,11 @@ const Auth = ({ title, children }: AuthProps) => (
   <S.Wrapper>
     <S.BannerBlock>
       <S.BannerContent>
-        <Logo id="banner" />
+        <Link href="/">
+          <a>
+            <Logo id="banner" />
+          </a>
+        </Link>
 
         <div>
           <Heading>All your favorite games in one place</Heading>
@@ -26,7 +32,11 @@ const Auth = ({ title, children }: AuthProps) => (
 
     <S.Content>
       <S.ContentWrapper>
-        <Logo color="black" size="large" id="content" />
+        <Link href="/">
+          <a>
+            <Logo color="black" size="large" id="content" />
+          </a>
+        </Link>
         <Heading color="black" lineColor="secondary" lineLeft>
           {title}
         </Heading>
