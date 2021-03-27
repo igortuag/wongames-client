f892ca07e31d93764cbf7618cbde298ab3ae7b49
Igor Tuag <igortuag@gmail.com>
2021-03-27 08:44:20 -0300

    add styles to form

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,9 +1,34 @@
-import * as S from './styles'
+import styled, { css } from 'styled-components'
+import { darken } from 'polished'
 
-const Form = () => (
-  <S.Wrapper>
-    <h1>Form</h1>
-  </S.Wrapper>
-)
+import * as TextFieldStyles from 'components/TextField/styles'
+import * as ButtonStyles from 'components/Button/styles'
 
-export default Form
+export const FormWrapper = styled.div`
+  ${({ theme }) => css`
+    ${TextFieldStyles.Wrapper} {
+      margin: ${theme.spacings.xxsmall} 0;
+    }
+    ${ButtonStyles.Wrapper} {
+      margin: ${theme.spacings.medium} auto ${theme.spacings.xsmall};
+    }
+  `}
+`
+
+export const FormLink = styled.div`
+  ${({ theme }) => css`
+    font-size: ${theme.font.sizes.small};
+    color: ${theme.colors.black};
+    text-align: center;
+    a {
+      color: ${theme.colors.secondary};
+      text-decoration: none;
+      border-bottom: 0.1rem solid ${theme.colors.secondary};
+      transition: color, border, ${theme.transition.fast};
+      &:hover {
+        color: ${darken(0.1, theme.colors.secondary)};
+        border-bottom: 0.1rem solid ${darken(0.1, theme.colors.secondary)};
+      }
+    }
+  `}
+`
