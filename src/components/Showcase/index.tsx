fcb89ac756d1145ded233b57289463c7679f53ef
Igor Tuag <igortuag@gmail.com>
2021-03-28 09:31:00 -0300

    add structure to showcase

--- src/components/Showcase/index.tsx
+++ src/components/Showcase/index.tsx
@@ -1,8 +1,15 @@
+import GameCardSlider from 'components/GameCardSlider'
+import Heading from 'components/Heading'
+import Highlight from 'components/Highlight'
 import * as S from './styles'
 
 const Showcase = () => (
   <S.Wrapper>
-    <h1>Showcase</h1>
+    <Heading lineLeft lineColor="secondary">
+      Free games
+    </Heading>
+    <Highlight {...freeHighlight} />
+    <GameCardSlider items={freeGames} />
   </S.Wrapper>
 )
 
