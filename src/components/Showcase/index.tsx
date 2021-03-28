eb83f0f515319b904d893006cf3360a750554731
Igor Tuag <igortuag@gmail.com>
2021-03-28 09:32:44 -0300

    add props to showcase

--- src/components/Showcase/index.tsx
+++ src/components/Showcase/index.tsx
@@ -1,9 +1,17 @@
+import { GameCardProps } from 'components/GameCard'
 import GameCardSlider from 'components/GameCardSlider'
 import Heading from 'components/Heading'
-import Highlight from 'components/Highlight'
+import Highlight, { HighlightProps } from 'components/Highlight'
+
 import * as S from './styles'
 
-const Showcase = () => (
+export type ShowcaseProps = {
+  title?: string
+  highlight?: HighlightProps
+  games?: GameCardProps
+}
+
+const Showcase = ({ title, highlight, games }: ShowcaseProps) => (
   <S.Wrapper>
     <Heading lineLeft lineColor="secondary">
       Free games
