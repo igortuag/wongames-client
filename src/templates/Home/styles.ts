086ca63bce88a8ffe7cee68d216ae2d78d0b3fc5
Igor Tuag <igortuag@gmail.com>
2021-03-29 07:52:25 -0300

    remove section styles on home

--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -5,28 +5,6 @@ import * as HeadingStyles from 'components/Heading/styles'
 import * as GameCardSliderStyles from 'components/GameCardSlider/styles'
 import * as HighlightStyles from 'components/Highlight/styles'
 
-const Sections = styled.section`
-  ${({ theme }) => css`
-    ${HeadingStyles.Wrapper},
-    ${HighlightStyles.Wrapper},
-    ${GameCardSliderStyles.Wrapper} {
-      margin-bottom: ${theme.spacings.medium};
-    }
-    ${HighlightStyles.Wrapper} {
-      ${media.lessThan('medium')`
-        margin-right: calc(-${theme.grid.gutter} / 2);
-        margin-left: calc(-${theme.grid.gutter} / 2);
-      `}
-    }
-    ${GameCardSliderStyles.Wrapper} {
-      ${media.lessThan('huge')`
-        margin-right: calc(-${theme.grid.gutter} / 2);
-      `}
-    }
-    margin-bottom: calc(${theme.spacings.large} * 2);
-  `}
-`
-
 export const SectionBanner = styled.section`
   ${({ theme }) => css`
     margin: ${theme.spacings.large} calc(-${theme.grid.gutter} / 2);
