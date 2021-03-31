f11e6cec617f71ac273a2b5d11292ca8f958abfd
Igor Tuag <igortuag@gmail.com>
2021-03-31 08:06:52 -0300

    add BaseTemplateProps props

--- src/templates/Base/index.tsx
+++ src/templates/Base/index.tsx
@@ -1,6 +1,10 @@
 import * as S from './styles'
 
-const Base = () => (
+export type BaseTemplateProps = {
+  children: React.ReactNode
+}
+
+const Base = ({ children }: BaseTemplateProps) => (
   <S.Wrapper>
     <h1>Base</h1>
   </S.Wrapper>
