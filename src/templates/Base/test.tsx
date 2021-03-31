c879dc2cdca36d14c5838bf928018ad4ce788532
Igor Tuag <igortuag@gmail.com>
2021-03-31 08:02:49 -0300

    add mocks components Menu and footer

--- src/templates/Base/test.tsx
+++ src/templates/Base/test.tsx
@@ -2,6 +2,24 @@ import { render, screen } from '@testing-library/react'
 
 import Base from '.'
 
+jest.mock(`components/Menu`, () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Mock Menu"></div>
+    }
+  }
+})
+
+jest.mock(`components/Footer`, () => {
+  return {
+    __esModule: true,
+    default: function Mock() {
+      return <div data-testid="Mock Footer"></div>
+    }
+  }
+})
+
 describe('<Base />', () => {
   it('should render the heading', () => {
     const { container } = render(<Base />)
