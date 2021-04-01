260763f21abc01de70d022ce33861fe5deb634e6
Igor Tuag <igortuag@gmail.com>
2021-04-01 07:25:36 -0300

    define tests about game information

--- src/components/GameInfo/test.tsx
+++ src/components/GameInfo/test.tsx
@@ -1,13 +1,14 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helper'
 
 import GameInfo from '.'
 
 describe('<GameInfo />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<GameInfo />)
+  it('should render game information', () => {
+    renderWithTheme(<GameInfo {...props} />)
 
-    expect(screen.getByRole('heading', { name: /GameInfo/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+    // esperar por um heading (title)
+    // esperar por um description
+    // esperar pelo price
   })
 })
