35520bf87009831ae66434edfebb96a57612df50
Igor Tuag <igortuag@gmail.com>
2021-04-03 09:56:04 -0300

    add parameters to stories

--- src/components/GameInfo/stories.tsx
+++ src/components/GameInfo/stories.tsx
@@ -5,6 +5,11 @@ import mockGame from './mock'
 export default {
   title: 'GameInfo',
   component: GameInfo,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  },
   args: mockGame
 } as Meta
 
