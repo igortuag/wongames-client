48eeaa1b60a59287d4187c4c048e4a08383bded0
Igor Tuag <igortuag@gmail.com>
2021-04-03 10:02:57 -0300

    edit RibbonStyles on infogame

--- src/components/GameInfo/styles.ts
+++ src/components/GameInfo/styles.ts
@@ -1,6 +1,8 @@
 import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
 
+import * as RibbonStyles from 'components/Ribbon/styles'
+
 export const Wrapper = styled.div`
   ${({ theme }) => css`
     position: relative;
@@ -8,8 +10,27 @@ export const Wrapper = styled.div`
     padding: ${theme.spacings.small};
     padding-top: ${theme.spacings.xxlarge};
 
+    ${RibbonStyles.Wrapper} {
+      right: -1rem;
+
+      &:before {
+        border-right-width: 1rem;
+      }
+    }
+
     ${media.greaterThan('small')`
       padding: ${theme.spacings.small};
+    `}
+
+    ${media.greaterThan('medium')`
+      ${RibbonStyles.Wrapper} {
+        right: ${theme.spacings.small};
+        top: ${theme.spacings.small};
+        front-size: ${theme.font.sizes.large};
+        &:before {
+          border: none;
+        }
+      }
     `}
   `}
 `
