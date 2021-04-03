fbe881680dfa3fbcc55cb53d28f9863e081aa296
Igor Tuag <igortuag@gmail.com>
2021-04-03 10:00:33 -0300

    add infoGame Wrapper styles

--- src/components/GameInfo/styles.ts
+++ src/components/GameInfo/styles.ts
@@ -1,6 +1,18 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    position: relative;
+    background: ${theme.colors.white};
+    padding: ${theme.spacings.small};
+    padding-top: ${theme.spacings.xxlarge};
+
+    ${media.greaterThan('small')`
+      padding: ${theme.spacings.small};
+    `}
+  `}
+`
 
 export const Description = styled.p``
 
