b38827ecf8eef07d1e3b1179d4429ac1dc8a5d46
Igor Tuag <igortuag@gmail.com>
2021-04-03 09:55:23 -0300

    add props, args and mock to stories

--- src/components/GameInfo/stories.tsx
+++ src/components/GameInfo/stories.tsx
@@ -1,9 +1,11 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import GameInfo from '.'
+import GameInfo, { GameInfoProps } from '.'
+import mockGame from './mock'
 
 export default {
   title: 'GameInfo',
-  component: GameInfo
+  component: GameInfo,
+  args: mockGame
 } as Meta
 
-export const Default: Story = () => <GameInfo />
+export const Default: Story<GameInfoProps> = (args) => <GameInfo {...args} />
