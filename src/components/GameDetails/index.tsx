c544e1569053f67ebc1de242131b5bea17ae026b
Igor Tuag <igortuag@gmail.com>
2021-04-04 16:40:08 -0300

    add structure to GameDetails

--- src/components/GameDetails/index.tsx
+++ src/components/GameDetails/index.tsx
@@ -9,6 +9,37 @@ const GameDetails = () => (
         Game Details
       </Heading>
     </MediaMatch>
+
+    <S.Content>
+      <S.Block>
+        <S.Label>Developer</S.Label>
+        <S.Description>Gearbox Software</S.Description>
+      </S.Block>
+
+      <S.Block>
+        <S.Label>Release Date</S.Label>
+        <S.Description>Nov 16. 2019</S.Description>
+      </S.Block>
+
+      <S.Block>
+        <S.Label>Platforms</S.Label>
+      </S.Block>
+
+      <S.Block>
+        <S.Label>Publisher</S.Label>
+        <S.Description>2k</S.Description>
+      </S.Block>
+
+      <S.Block>
+        <S.Label>Rating</S.Label>
+        <S.Description>18+</S.Description>
+      </S.Block>
+
+      <S.Block>
+        <S.Label>Genres</S.Label>
+        <S.Description>Action / Adventure</S.Description>
+      </S.Block>
+    </S.Content>
   </S.Wrapper>
 )
 
