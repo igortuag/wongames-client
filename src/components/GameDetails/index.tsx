a161bae9ba6ef653d6215a4e49ecd45d5f05853e
Igor Tuag <igortuag@gmail.com>
2021-04-04 16:32:57 -0300

    add MediaMatch to GameDetails to hidden heading

--- src/components/GameDetails/index.tsx
+++ src/components/GameDetails/index.tsx
@@ -1,11 +1,14 @@
 import Heading from 'components/Heading'
+import MediaMatch from 'components/MediaMatch'
 import * as S from './styles'
 
 const GameDetails = () => (
   <S.Wrapper>
-    <Heading lineLeft lineColor="secondary">
-      Game Details
-    </Heading>
+    <MediaMatch greaterThan="small">
+      <Heading lineLeft lineColor="secondary">
+        Game Details
+      </Heading>
+    </MediaMatch>
   </S.Wrapper>
 )
 
