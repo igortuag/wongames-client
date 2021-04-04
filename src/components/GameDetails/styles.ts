48155a387e30dd107dc5657e3cd37dc38ce70bcf
Igor Tuag <igortuag@gmail.com>
2021-04-04 16:36:26 -0300

    add structure styles GameDetails

--- src/components/GameDetails/styles.ts
+++ src/components/GameDetails/styles.ts
@@ -1,3 +1,11 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.div``
+
+export const Content = styled.div``
+
+export const Block = styled.div``
+
+export const Label = styled.h3``
+
+export const Description = styled.p``
