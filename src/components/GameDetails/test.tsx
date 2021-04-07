ad71a3a16d91d0b3b58871e39676b59f7e56ac22
Igor Tuag <igortuag@gmail.com>
2021-04-07 08:18:12 -0300

    should render the blocks tests

--- src/components/GameDetails/test.tsx
+++ src/components/GameDetails/test.tsx
@@ -1,13 +1,35 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helper'
 
-import GameDetails from '.'
+import GameDetails, { GameDetailsProps } from '.'
+
+const props: GameDetailsProps = {
+  developer: 'Diferente Tales',
+  platforms: ['windows', 'mac', 'linux']
+}
 
 describe('<GameDetails />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<GameDetails />)
+  it('should render the blocks', () => {
+    renderWithTheme(<GameDetails {...props} />)
 
-    expect(screen.getByRole('heading', { name: /GameDetails/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /developer/i })
+    ).toBeInTheDocument()
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(
+      screen.getByRole('heading', { name: /release date/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /platforms/i })
+    ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: /publisher/i })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /rating/i })).toBeInTheDocument()
+
+    expect(screen.getByRole('heading', { name: /genres/i })).toBeInTheDocument()
   })
 })
