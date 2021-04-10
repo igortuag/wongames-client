2f860354efc1720b7fcdbfbf16f585039290652e
Igor Tuag <igortuag@gmail.com>
2021-04-10 08:05:50 -0300

    generate textContent

---
+++ src/components/TextContent/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+const TextContent = () => (
+  <S.Wrapper>
+    <h1>TextContent</h1>
+  </S.Wrapper>
+)
+
+export default TextContent
---
+++ src/components/TextContent/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import TextContent from '.'
+
+export default {
+  title: 'TextContent',
+  component: TextContent
+} as Meta
+
+export const Default: Story = () => <TextContent />
