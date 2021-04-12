a3f3321ab07def46052c409408e1e70128e8cd94
Igor Tuag <igortuag@gmail.com>
2021-04-12 07:49:07 -0300

    should render white color in desktop and black color in mobile test

--- src/components/TextContent/test.tsx
+++ src/components/TextContent/test.tsx
@@ -27,4 +27,19 @@ describe('<TextContent />', () => {
       screen.queryByRole('heading', { name: /description/i })
     ).not.toBeInTheDocument()
   })
+
+  it('should render white color in desktop and black color in mobile', () => {
+    renderWithTheme(<TextContent {...props} />)
+
+    const wrapper = screen.getByRole('heading', { name: /description/i })
+      .parentElement
+
+    expect(wrapper).toHaveStyle({
+      color: '#FAFAFA'
+    })
+
+    expect(wrapper).toHaveStyleRule('color', '#030517', {
+      media: '(min-width: 768px)'
+    })
+  })
 })
