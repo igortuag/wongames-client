dfc87a43debe2d0003cf2032cbf4d29892eabf78
Igor Tuag <igortuag@gmail.com>
2021-04-14 08:02:09 -0300

    config stories parameters

--- src/components/Gallery/stories.tsx
+++ src/components/Gallery/stories.tsx
@@ -1,9 +1,16 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
 import Gallery from '.'
+import items from './mock'
 
 export default {
   title: 'Gallery',
-  component: Gallery
+  component: Gallery,
+  args: {
+    items
+  },
+  parameters: {
+    layout: 'fullscreen'
+  }
 } as Meta
 
 export const Default: Story = () => <Gallery />
