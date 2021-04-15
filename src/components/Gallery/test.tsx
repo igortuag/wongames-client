901cd98deae5b44864543ff6d4ac9a5c2d432828
Igor Tuag <igortuag@gmail.com>
2021-04-15 08:22:51 -0300

    should handle open/close modal test

--- src/components/Gallery/test.tsx
+++ src/components/Gallery/test.tsx
@@ -1,5 +1,5 @@
 import 'match-media-mock'
-import { screen } from '@testing-library/react'
+import { fireEvent, screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helper'
 
 import Gallery from '.'
@@ -16,5 +16,26 @@ describe('<Gallery />', () => {
     expect(
       screen.getByRole('button', { name: /Thumb - Gallery Image 2/i })
     ).toHaveAttribute('src', mockItems[1].src)
+  })
+
+  it('should handle open/close modal ', () => {
+    renderWithTheme(<Gallery items={mockItems.slice(0, 2)} />)
+
+    // selecionar Modal
+    const modal = screen.getByLabelText('modal')
+
+    // verificar se o menu esta escondido
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({ opacity: 0 })
+
+    // clicar no botão de abrir menu e verificar se abriu
+    fireEvent.click(screen.getByLabelText(/open menu/i))
+    expect(modal.getAttribute('aria-hidden')).toBe('false')
+    expect(modal).toHaveStyle({ opacity: 1 })
+
+    // clica no botão de fechar menu e verificar se fechou
+    fireEvent.click(screen.getByLabelText(/close menu/i))
+    expect(modal.getAttribute('aria-hidden')).toBe('true')
+    expect(modal).toHaveStyle({ opacity: 0 })
   })
 })
