e81918d0d45afad468f87c837b3d0551b3671878
Igor Tuag <igortuag@gmail.com>
2021-04-17 07:31:16 -0300

    add event to listener key up esc

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { ArrowBackIos as ArrowLeft } from '@styled-icons/material-outlined/ArrowBackIos'
 import { ArrowForwardIos as ArrowRight } from '@styled-icons/material-outlined/ArrowForwardIos'
 import { Close } from '@styled-icons/material-outlined/Close'
@@ -6,6 +6,7 @@ import { Close } from '@styled-icons/material-outlined/Close'
 import Slider, { SliderSettings } from 'components/Slider'
 
 import * as S from './styles'
+import { cleanup } from '@testing-library/react'
 
 const settings: SliderSettings = {
   arrows: true,
@@ -53,6 +54,15 @@ export type GalleryProps = {
 
 const Gallery = ({ items }: GalleryProps) => {
   const [isOpen, setIsOpen] = useState(false)
+
+  useEffect(() => {
+    const handleKeyUp = ({ key }: KeyboardEvent) => {
+      key === 'Escape' && setIsOpen(false)
+    }
+
+    window.addEventListener('keyup', handleKeyUp)
+    return () => window.removeEventListener('keyup', handleKeyUp)
+  }, [])
 
   return (
     <S.Wrapper>
