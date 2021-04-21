83180128eaad64394dff0c65aca987e304cdf2fb
Igor Tuag <igortuag@gmail.com>
2021-04-21 08:08:46 -0300

    remove stories from game template

--- src/templates/Game/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Story, Meta } from '@storybook/react/types-6-0'
-import Game from '.'
-
-export default {
-  title: 'Game',
-  component: Game
-} as Meta
-
-export const Default: Story = () => <Game />
