98304cd43c078f9deaf08c9c2643efca72832331
Igor Tuag <igortuag@gmail.com>
2021-04-23 07:21:42 -0300

    add styles cover to mobile

--- src/templates/Game/styles.ts
+++ src/templates/Game/styles.ts
@@ -1,9 +1,19 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 type CoverProps = {
   src: string
 }
 
 export const Cover = styled.div<CoverProps>`
-  ${({ src }) => css``}
+  ${({ src }) => css`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 39.5rem;
+    background-image: url(${src});
+    background-size: cover;
+    background-position: top center;
+    opacity: 0.4;
+  `}
 `
