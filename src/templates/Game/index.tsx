178bdb31455f94653c0f4f671fd539430430553a
Igor Tuag <igortuag@gmail.com>
2021-04-26 07:59:27 -0300

    add GalleryImageProps

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,3 +1,4 @@
+import { GalleryImageProps } from 'components/Gallery'
 import GameInfo, { GameInfoProps } from 'components/GameInfo'
 import Base from 'templates/Base'
 
@@ -6,9 +7,10 @@ import * as S from './styles'
 export type GameTemplateProps = {
   cover: string
   gameInfo: GameInfoProps
+  gallery?: GalleryImageProps
 }
 
-const Game = ({ cover, gameInfo }: GameTemplateProps) => (
+const Game = ({ cover, gameInfo, gallery }: GameTemplateProps) => (
   <Base>
     <S.Cover src={cover} role="image" aria-label="cover"></S.Cover>
     <S.Main>
