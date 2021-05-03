2ebb47538e3e825a1666f6d093912e9d0e745560
Igor Tuag <igortuag@gmail.com>
2021-05-03 07:31:59 -0300

    add Showcaseto wishlist template

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -4,20 +4,30 @@ import { Container } from 'components/Container'
 import { GameCardProps } from 'components/GameCard'
 import { HighlightProps } from 'components/Highlight'
 import Heading from 'components/Heading'
+import Showcase from 'components/Showcase'
 
 import * as S from './styles'
 
 export type WishlistTemplateProps = {
-  recommendedGames: GameCardProps
+  recommendedGames: GameCardProps[]
   recommendedHighlight: HighlightProps
 }
 
-const Wishlist = () => (
+const Wishlist = ({
+  recommendedGames,
+  recommendedHighlight
+}: WishlistTemplateProps) => (
   <Base>
     <Container>
       <Heading lineLeft lineColor="secondary">
         Wishlist
       </Heading>
+
+      <Showcase
+        title="You may like these games"
+        games={recommendedGames}
+        highlight={recommendedHighlight}
+      />
     </Container>
   </Base>
 )
