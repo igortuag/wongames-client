9359d65fce7930bf02a18fc32607c5cf6e032452
Igor Tuag <igortuag@gmail.com>
2021-05-03 07:29:53 -0300

    add WishlistTemplateProps

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -1,8 +1,16 @@
 import Base from 'templates/Base'
+
 import { Container } from 'components/Container'
+import { GameCardProps } from 'components/GameCard'
+import { HighlightProps } from 'components/Highlight'
 import Heading from 'components/Heading'
 
 import * as S from './styles'
+
+export type WishlistTemplateProps = {
+  recommendedGames: GameCardProps
+  recommendedHighlight: HighlightProps
+}
 
 const Wishlist = () => (
   <Base>
