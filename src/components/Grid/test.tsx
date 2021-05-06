e5b2b7779bb394ef7032d3761d6068d5ebe773ca
Igor Tuag <igortuag@gmail.com>
2021-05-06 08:39:27 -0300

    add sapshot to grid

--- src/components/Grid/test.tsx
+++ src/components/Grid/test.tsx
@@ -4,8 +4,21 @@ import { Grid } from '.'
 
 describe('<Grid />', () => {
   it('should render correctly', () => {
-    const { container } = renderWithTheme(<Grid />)
+    const { container } = renderWithTheme(<Grid>Lorem Ipsum</Grid>)
 
-    expect(container.firstChild).toMatchInlineSnapshot()
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
+        display: grid;
+        grid-template-columns: repeat(auto-fill,minmax(25rem,1fr));
+        margin: 3.2rem 0;
+        gap: 3.2rem;
+      }
+
+      <div
+        class="c0"
+      >
+        Lorem Ipsum
+      </div>
+    `)
   })
 })
