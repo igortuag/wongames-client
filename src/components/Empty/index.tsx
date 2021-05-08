071daacf324d2ec516476a72b9aff1d027f859cb
Igor Tuag <igortuag@gmail.com>
2021-05-08 08:25:57 -0300

    render a image correctly

--- src/components/Empty/index.tsx
+++ src/components/Empty/index.tsx
@@ -2,7 +2,11 @@ import * as S from './styles'
 
 const Empty = () => (
   <S.Wrapper>
-    <h1>Empty</h1>
+    <S.Image
+      src="/img/empty.svg"
+      alt="a gamer in a couch playing videogame"
+      role="image"
+    ></S.Image>
   </S.Wrapper>
 )
 
