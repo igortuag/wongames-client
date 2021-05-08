64a0e5a5ef95664ad9834ddc48b8fbe1e4dca533
Igor Tuag <igortuag@gmail.com>
2021-05-08 08:38:18 -0300

    show link when hasLink true

--- src/components/Empty/index.tsx
+++ src/components/Empty/index.tsx
@@ -1,3 +1,5 @@
+import Button from 'components/Button'
+import Link from 'next/link'
 import * as S from './styles'
 
 export type EmptyProps = {
@@ -15,6 +17,12 @@ const Empty = ({ title, description, hasLink }: EmptyProps) => (
     />
     <S.Title>{title}</S.Title>
     <S.Description>{description}</S.Description>
+
+    {hasLink && (
+      <Link href="/" passHref>
+        <Button as="a">Go back to store</Button>
+      </Link>
+    )}
   </S.Wrapper>
 )
 
--- src/components/Empty/test.tsx
+++ src/components/Empty/test.tsx
@@ -10,7 +10,7 @@ const props = {
 
 describe('<Empty />', () => {
   it('should render correctly', () => {
-    renderWithTheme(<Empty {...props} />)
+    renderWithTheme(<Empty {...props} hasLink />)
 
     expect(
       screen.getByRole('image', {
