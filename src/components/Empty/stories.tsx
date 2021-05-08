bdcb0c1b16f8c000feb230ca0040f28a3fb9ca56
Igor Tuag <igortuag@gmail.com>
2021-05-08 08:33:27 -0300

    config empty storybook

--- src/components/Empty/stories.tsx
+++ src/components/Empty/stories.tsx
@@ -1,9 +1,19 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import Empty from '.'
+import Empty, { EmptyProps } from '.'
 
 export default {
   title: 'Empty',
-  component: Empty
+  component: Empty,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
 } as Meta
 
-export const Default: Story = () => <Empty />
+export const Default: Story<EmptyProps> = (args) => <Empty {...args} />
+
+Default.args = {
+  title: 'Your wishlist is empty',
+  description: 'Games added to your wishlist will appear here'
+}
