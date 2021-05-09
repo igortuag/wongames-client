dfc96b3a43d53c538f270e8ff13a304678ec9d8c
Igor Tuag <igortuag@gmail.com>
2021-05-09 09:15:17 -0300

    add styles to empty Wrapper

--- src/components/Empty/styles.ts
+++ src/components/Empty/styles.ts
@@ -1,6 +1,11 @@
 import styled from 'styled-components'
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`
 
 export const Image = styled.img``
 
