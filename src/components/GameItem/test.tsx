1f25ea1877c0907413591b19cc80f1cf5754ddfe
Igor Tuag <igortuag@gmail.com>
2021-05-10 08:13:36 -0300

    should render the item test

--- src/components/GameItem/test.tsx
+++ src/components/GameItem/test.tsx
@@ -1,13 +1,27 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helper'
 
 import GameItem from '.'
 
+const props = {
+  img: 'https://source.unsplash.com/user/willianjusten/151x70',
+  title: 'Red Dead Redemption 2',
+  price: 'R$ 215,00'
+}
+
 describe('<GameItem />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<GameItem />)
+  it('should render the item', () => {
+    renderWithTheme(<GameItem {...props} />)
 
-    expect(screen.getByRole('heading', { name: /GameItem/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: props.title })
+    ).toBeInTheDocument()
 
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      props.img
+    )
+
+    expect(screen.getByText('R$ 215,00')).toBeInTheDocument()
   })
 })
