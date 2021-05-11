8d9d07fd95b4bc8602b0606e977fab80f60f2a1a
Igor Tuag <igortuag@gmail.com>
2021-05-11 08:23:55 -0300

    add styles to content

--- src/components/GameItem/styles.ts
+++ src/components/GameItem/styles.ts
@@ -37,7 +37,11 @@ export const ImageBox = styled.div`
   `}
 `
 
-export const Content = styled.div``
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`
 
 export const Title = styled.h3``
 
