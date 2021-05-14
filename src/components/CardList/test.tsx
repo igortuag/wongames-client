93029f80c1c86f02ebfa76893aa936b46b080a4d
Igor Tuag <igortuag@gmail.com>
2021-05-14 08:18:22 -0300

    generate CardList

---
+++ src/components/CardList/test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react'
+
+import CardList from '.'
+
+describe('<CardList />', () => {
+  it('should render the heading', () => {
+    const { container } = render(<CardList />)
+
+    expect(screen.getByRole('heading', { name: /CardList/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
