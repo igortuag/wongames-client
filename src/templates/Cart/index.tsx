d4755b04a6ad94d469e1d38e146be6ef27335bae
Igor Tuag <igortuag@gmail.com>
2021-05-18 07:56:52 -0300

    add showcase and cartProps to cart template

--- src/templates/Cart/index.tsx
+++ src/templates/Cart/index.tsx
@@ -1,15 +1,31 @@
+import { Divider } from 'components/Divider'
+import { GameCardProps } from 'components/GameCard'
 import Heading from 'components/Heading'
+import { HighlightProps } from 'components/Highlight'
+import Showcase from 'components/Showcase'
 import { Container } from 'next/app'
 import Base from 'templates/Base'
 import * as S from './styles'
 
-const Cart = () => (
+export type CartProps = {
+  recommendedGames: GameCardProps[]
+  recommendedHighlight: HighlightProps
+}
+
+const Cart = ({ recommendedGames, recommendedHighlight }: CartProps) => (
   <Base>
     <Container>
       <Heading lineLeft lineColor="secondary">
         My Cart
       </Heading>
+      <Divider />
     </Container>
+
+    <Showcase
+      title="You may like these games"
+      games={recommendedGames}
+      highlight={recommendedHighlight}
+    />
   </Base>
 )
 
