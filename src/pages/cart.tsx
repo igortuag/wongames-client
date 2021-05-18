f5dcd5b0447df32a87f1148a97b8858567e4784b
Igor Tuag <igortuag@gmail.com>
2021-05-18 08:02:58 -0300

    add itemsMock to cart page

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,5 +1,6 @@
 import Cart, { CartProps } from 'templates/Cart'
 
+import itemsMock from 'components/CardList/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
 
@@ -10,6 +11,7 @@ export default function CartPage(props: CartProps) {
 export async function getServerSideProps() {
   return {
     props: {
+      items: itemsMock,
       recommendedGames: gamesMock.slice(0, 5),
       recommendedHighlights: highlightMock
     }
