ea290ca09eba7a9147866a6c57a5d8e1927cf5f9
Igor Tuag <igortuag@gmail.com>
2021-05-19 07:18:58 -0300

    add props to test

--- src/templates/Cart/test.tsx
+++ src/templates/Cart/test.tsx
@@ -1,6 +1,19 @@
 import { render, screen } from '@testing-library/react'
 
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+import itemsMock from 'components/CardList/mock'
+import cardsMock from 'components/PaymentOptions/mock'
+
 import Cart from '.'
+
+const props = {
+  items: itemsMock,
+  total: '$ 430,00',
+  cards: cardsMock,
+  recommendedHighlight: highlightMock,
+  recommendedGames: gamesMock
+}
 
 describe('<Cart />', () => {
   it('should render the heading', () => {
