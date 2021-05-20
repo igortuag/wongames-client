f1905a2df5473f639ac10d2ed8417c3acfdab5e7
Igor Tuag <igortuag@gmail.com>
2021-05-20 08:16:33 -0300

    add AccountCircle

--- src/components/ProfileMenu/index.tsx
+++ src/components/ProfileMenu/index.tsx
@@ -1,10 +1,13 @@
+import { AccountCircle } from '@styled-icons/material-outlined'
 import Link from 'next/link'
 import * as S from './styles'
 
 const ProfileMenu = () => (
   <S.Nav>
     <Link href="/profile/me" passHref>
-      <S.Link></S.Link>
+      <S.Link>
+        <AccountCircle />
+      </S.Link>
     </Link>
   </S.Nav>
 )
