c137c96ade281dc462a177044ad134f76c367867
Igor Tuag <igortuag@gmail.com>
2021-05-20 08:12:14 -0300

    generate profileMenu

---
+++ src/components/ProfileMenu/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+const ProfileMenu = () => (
+  <S.Wrapper>
+    <h1>ProfileMenu</h1>
+  </S.Wrapper>
+)
+
+export default ProfileMenu
---
+++ src/components/ProfileMenu/test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from '@testing-library/react'
+
+import ProfileMenu from '.'
+
+describe('<ProfileMenu />', () => {
+  it('should render the heading', () => {
+    const { container } = render(<ProfileMenu />)
+
+    expect(screen.getByRole('heading', { name: /ProfileMenu/i })).toBeInTheDocument()
+
+    expect(container.firstChild).toMatchSnapshot()
+  })
+})
