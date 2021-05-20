2339edf14a3b168811aad96c364cdfb1cd17245e
Igor Tuag <igortuag@gmail.com>
2021-05-20 08:15:27 -0300

    add structure link

--- src/components/ProfileMenu/index.tsx
+++ src/components/ProfileMenu/index.tsx
@@ -1,5 +1,12 @@
+import Link from 'next/link'
 import * as S from './styles'
 
-const ProfileMenu = () => <S.Nav></S.Nav>
+const ProfileMenu = () => (
+  <S.Nav>
+    <Link href="/profile/me" passHref>
+      <S.Link></S.Link>
+    </Link>
+  </S.Nav>
+)
 
 export default ProfileMenu
