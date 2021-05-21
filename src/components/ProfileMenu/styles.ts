27c0537dd779f7ed457b42b4ca13e9af1eb4f3e3
Igor Tuag <igortuag@gmail.com>
2021-05-21 08:21:44 -0300

    add styles to nav on profilemenu

--- src/components/ProfileMenu/styles.ts
+++ src/components/ProfileMenu/styles.ts
@@ -1,5 +1,18 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
-export const Nav = styled.nav``
+export const Nav = styled.nav`
+  ${({ theme }) => css`
+    display: flex;
+    border-bottom: 0.1rem solid ${theme.colors.lightGray};
+    ${media.greaterThan('medium')`
+      flex-direction: column;
+      border: 0;
+      a:not(:last-child) {
+        border-bottom: 0.1rem solid ${theme.colors.lightGray};
+      }
+    `}
+  `}
+`
 
 export const Link = styled.a``
