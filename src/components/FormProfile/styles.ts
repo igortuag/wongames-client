3c589e55e7ac65b8e1e4bc87f11c62e41a72eb01
Igor Tuag <igortuag@gmail.com>
2021-05-25 07:51:02 -0300

    add styles to form on desktop

--- src/components/FormProfile/styles.ts
+++ src/components/FormProfile/styles.ts
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 export const Wrapper = styled.main``
 
@@ -11,5 +12,15 @@ export const Form = styled.form`
     > button {
       margin-top: ${theme.spacings.xxlarge};
     }
+
+    ${media.greaterThan('medium')`
+      grid-template-columns: 1fr 1fr;
+      gap: ${theme.spacings.medium};
+      > button {
+        grid-column: 2;
+        justify-self: end;
+        margin-top: 0;
+      }
+    `}
   `}
 `
