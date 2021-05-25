1039031469a3f2a289a10d70caea2024763381d8
Igor Tuag <igortuag@gmail.com>
2021-05-25 07:50:45 -0300

    add styles to form profile on mobile

--- src/components/FormProfile/styles.ts
+++ src/components/FormProfile/styles.ts
@@ -1,5 +1,15 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Wrapper = styled.main``
 
-export const Form = styled.form``
+export const Form = styled.form`
+  ${({ theme }) => css`
+    max-width: 100%;
+    display: grid;
+    gap: ${theme.spacings.xsmall};
+
+    > button {
+      margin-top: ${theme.spacings.xxlarge};
+    }
+  `}
+`
