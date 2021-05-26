a1526fe159e189d2015cc21ba0a120dc840870d7
Igor Tuag <igortuag@gmail.com>
2021-05-26 07:42:06 -0300

    add Heading to cardsList

--- src/components/CardsList/index.tsx
+++ src/components/CardsList/index.tsx
@@ -1,8 +1,11 @@
+import Heading from 'components/Heading'
 import * as S from './styles'
 
 const CardsList = () => (
   <S.Wrapper>
-    <h1>CardsList</h1>
+    <Heading lineBottom color="black" size="small">
+      My Cards
+    </Heading>
   </S.Wrapper>
 )
 
