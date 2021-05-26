98b6702238af0632d2fb67ef681b4940f2d19027
Igor Tuag <igortuag@gmail.com>
2021-05-26 07:47:36 -0300

    config cardList props stories

--- src/components/CardsList/stories.tsx
+++ src/components/CardsList/stories.tsx
@@ -1,9 +1,14 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import CardsList from '.'
+import CardsList, { CardsListProps } from '.'
+
+import cardsMock from 'components/PaymentOptions/mock'
 
 export default {
-  title: 'CardsList',
-  component: CardsList
+  title: 'Profile/CardsList',
+  component: CardsList,
+  args: {
+    cards: cardsMock
+  }
 } as Meta
 
-export const Default: Story = () => <CardsList />
+export const Default: Story<CardsListProps> = (args) => <CardsList {...args} />
