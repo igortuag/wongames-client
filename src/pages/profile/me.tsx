f8451442fa4deec36379bb7454774c7ea8104f21
Igor Tuag <igortuag@gmail.com>
2021-05-27 08:17:53 -0300

    add FormProfile

--- src/pages/profile/me.tsx
+++ src/pages/profile/me.tsx
@@ -1,5 +1,10 @@
+import FormProfile from 'components/FormProfile'
 import Profile from 'templates/Profile'
 
 export default function Me() {
-  return <Profile />
+  return (
+    <Profile>
+      <FormProfile />
+    </Profile>
+  )
 }
