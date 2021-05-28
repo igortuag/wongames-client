37b11d1873548ef8dada8bda3dc733092433c3e6
Igor Tuag <igortuag@gmail.com>
2021-05-28 08:15:00 -0300

    add mockCards from cards mock

--- src/pages/profile/cards.tsx
+++ src/pages/profile/cards.tsx
@@ -1,10 +1,20 @@
 import CardsList from 'components/CardsList'
 import Profile from 'templates/Profile'
 
+import mockCards from 'components/PaymentOptions/mock'
+
 export default function Cards() {
   return (
     <Profile>
       <CardsList />
     </Profile>
   )
+}
+
+export function getServerSideProps() {
+  return {
+    props: {
+      cards: mockCards
+    }
+  }
 }
