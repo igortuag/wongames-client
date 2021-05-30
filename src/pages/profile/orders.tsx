65a4215a41d1fa783de6a94f363ca877d86a2e54
Igor Tuag <igortuag@gmail.com>
2021-05-30 09:04:53 -0300

    add OrdersList to orders page

--- src/pages/profile/orders.tsx
+++ src/pages/profile/orders.tsx
@@ -1,5 +1,16 @@
+import OrdersList, { OrdersListProps } from 'components/OrdersList'
 import Profile from 'templates/Profile'
 
-export default function Orders() {
-  return <Profile />
+export default function Cards({ items }: OrdersListProps) {
+  return (
+    <Profile>
+      <OrdersList items={items} />
+    </Profile>
+  )
+}
+
+export function getServerSideProps() {
+  return {
+    props: {}
+  }
 }
