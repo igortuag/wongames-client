e435c2f9a585eff35f11a07582be57b81cb82cf5
Igor Tuag <igortuag@gmail.com>
2021-05-31 08:03:19 -0300

    add router mock

--- src/templates/Profile/test.tsx
+++ src/templates/Profile/test.tsx
@@ -1,12 +1,20 @@
-import { render, screen } from '@testing-library/react'
+import 'match-media-mock'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Profile from '.'
+
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(() => ({ asPath: '/profile/me' }))
+}))
 
 describe('<Profile />', () => {
   it('should render the heading', () => {
     const { container } = render(<Profile />)
 
-    expect(screen.getByRole('heading', { name: /Profile/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /Profile/i })
+    ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
   })
