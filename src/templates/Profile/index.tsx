dc2e3d9b162ffcd0c47a794e860e2ed89788c1e8
Igor Tuag <igortuag@gmail.com>
2021-05-31 08:01:31 -0300

    add asPath to template profile

--- src/templates/Profile/index.tsx
+++ src/templates/Profile/index.tsx
@@ -10,20 +10,24 @@ export type ProfileTemplateProps = {
   children: React.ReactNode
 }
 
-const Profile = ({ children }: ProfileTemplateProps) => (
-  <Base>
-    <Container>
-      <Heading lineLeft lineColor="secondary">
-        My profile
-      </Heading>
+const Profile = ({ children }: ProfileTemplateProps) => {
+  const { asPath } = useRouter()
 
-      <S.Main>
-        <ProfileMenu />
+  return (
+    <Base>
+      <Container>
+        <Heading lineLeft lineColor="secondary">
+          My profile
+        </Heading>
 
-        <S.Content>{children}</S.Content>
-      </S.Main>
-    </Container>
-  </Base>
-)
+        <S.Main>
+          <ProfileMenu />
+
+          <S.Content>{children}</S.Content>
+        </S.Main>
+      </Container>
+    </Base>
+  )
+}
 
 export default Profile
