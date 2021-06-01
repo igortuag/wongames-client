7fc4d0078f9458c1338255af8c2e4cac6b779e1b
Igor Tuag <igortuag@gmail.com>
2021-06-01 08:06:04 -0300

    Should render headings system and genre

--- src/components/ExploreSidebar/test.tsx
+++ src/components/ExploreSidebar/test.tsx
@@ -4,12 +4,14 @@ import { renderWithTheme } from 'utils/tests/helper'
 import ExploreSidebar from '.'
 
 describe('<ExploreSidebar />', () => {
-  it('should render the headings', () => {
+  it('Should render headings', () => {
     renderWithTheme(<ExploreSidebar />)
 
     expect(screen.getByRole('heading', { name: /price/i })).toBeInTheDocument()
     expect(
       screen.getByRole('heading', { name: /sort by/i })
     ).toBeInTheDocument()
+    expect(screen.getByRole('system', { name: /sort by/i })).toBeInTheDocument()
+    expect(screen.getByRole('genre', { name: /sort by/i })).toBeInTheDocument()
   })
 })
