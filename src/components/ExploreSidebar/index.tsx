8a16835d8308f366fa1dcda285058b4ae69c5852
Igor Tuag <igortuag@gmail.com>
2021-06-01 08:14:20 -0300

    add all Checkboxs

--- src/components/ExploreSidebar/index.tsx
+++ src/components/ExploreSidebar/index.tsx
@@ -8,6 +8,11 @@ const ExploreSidebar = () => (
       Price
     </Heading>
     <Checkbox name="under-50" label="under $50" labelFor="under-50" />
+    <Checkbox name="under-100" label="under $100" labelFor="under-100" />
+    <Checkbox name="under-150" label="under $150" labelFor="under-150" />
+    <Checkbox name="under-200" label="under $200" labelFor="under-200" />
+    <Checkbox name="free" label="Free" labelFor="free" />
+    <Checkbox name="discounted" label="Discounted" labelFor="discounted" />
     <Heading lineBottom lineColor="secondary" size="small">
       Sort by
     </Heading>
