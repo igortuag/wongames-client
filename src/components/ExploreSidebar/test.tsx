0e968684f120526b8252187b8272a66b202e57cd
Igor Tuag <igortuag@gmail.com>
2021-06-01 08:10:00 -0300

    Should render input checkbox under $50

--- src/components/ExploreSidebar/test.tsx
+++ src/components/ExploreSidebar/test.tsx
@@ -14,4 +14,12 @@ describe('<ExploreSidebar />', () => {
     expect(screen.getByRole('heading', { name: /system/i })).toBeInTheDocument()
     expect(screen.getByRole('heading', { name: /genre/i })).toBeInTheDocument()
   })
+
+  it('Should render inputs', () => {
+    renderWithTheme(<ExploreSidebar />)
+
+    expect(
+      screen.getByRole('checkbox', { name: /under \$50/i })
+    ).toBeInTheDocument()
+  })
 })
