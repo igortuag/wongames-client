d077148d58215c731c0166097b682e276aa1c74c
Igor Tuag <igortuag@gmail.com>
2021-06-01 08:06:28 -0300

    add Heading system and genre

--- src/components/ExploreSidebar/index.tsx
+++ src/components/ExploreSidebar/index.tsx
@@ -9,6 +9,12 @@ const ExploreSidebar = () => (
     <Heading lineBottom lineColor="secondary" size="small">
       Sort by
     </Heading>
+    <Heading lineBottom lineColor="secondary" size="small">
+      System
+    </Heading>
+    <Heading lineBottom lineColor="secondary" size="small">
+      Genre
+    </Heading>
   </S.Wrapper>
 )
 
