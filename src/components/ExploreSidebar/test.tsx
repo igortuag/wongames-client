50b22d4f5760050ea9e8be2cbe22926e002034b2
Igor Tuag <igortuag@gmail.com>
2021-06-02 08:14:55 -0300

    add mock to tests exploreSideBar

--- src/components/ExploreSidebar/test.tsx
+++ src/components/ExploreSidebar/test.tsx
@@ -1,11 +1,16 @@
 import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import { renderWithTheme } from 'utils/tests/helper'
+import { css } from 'styled-components'
 
 import ExploreSidebar from '.'
+import { Overlay } from './styles'
+
+import items from './mock'
 
 describe('<ExploreSidebar />', () => {
-  it('Should render headings', () => {
-    renderWithTheme(<ExploreSidebar />)
+  it('should render headings', () => {
+    renderWithTheme(<ExploreSidebar items={items} onFilter={jest.fn} />)
 
     expect(screen.getByRole('heading', { name: /price/i })).toBeInTheDocument()
     expect(
@@ -15,21 +20,107 @@ describe('<ExploreSidebar />', () => {
     expect(screen.getByRole('heading', { name: /genre/i })).toBeInTheDocument()
   })
 
-  it('Should render inputs', () => {
-    renderWithTheme(<ExploreSidebar />)
+  it('should render inputs', () => {
+    renderWithTheme(<ExploreSidebar items={items} onFilter={jest.fn} />)
 
     expect(
       screen.getByRole('checkbox', { name: /under \$50/i })
     ).toBeInTheDocument()
 
     expect(
-      screen.getByRole('radio', { name: /low to hight/i })
+      screen.getByRole('radio', { name: /low to high/i })
     ).toBeInTheDocument()
   })
 
-  it('Should render the filter button', () => {
-    renderWithTheme(<ExploreSidebar />)
+  it('should render the filter button', () => {
+    renderWithTheme(<ExploreSidebar items={items} onFilter={jest.fn} />)
 
     expect(screen.getByRole('button', { name: /filter/i })).toBeInTheDocument()
+  })
+
+  it('should check initial values that are passed', () => {
+    renderWithTheme(
+      <ExploreSidebar
+        items={items}
+        onFilter={jest.fn}
+        initialValues={{ windows: true, sort_by: 'low-to-high' }}
+      />
+    )
+
+    expect(screen.getByRole('checkbox', { name: /windows/i })).toBeChecked()
+
+    expect(screen.getByRole('radio', { name: /low to high/i })).toBeChecked()
+  })
+
+  it('should filter with initial values', () => {
+    const onFilter = jest.fn()
+
+    renderWithTheme(
+      <ExploreSidebar
+        items={items}
+        initialValues={{ windows: true, sort_by: 'low-to-high' }}
+        onFilter={onFilter}
+      />
+    )
+
+    userEvent.click(screen.getByRole('button', { name: /filter/i }))
+
+    expect(onFilter).toBeCalledWith({ windows: true, sort_by: 'low-to-high' })
+  })
+
+  it('should filter with checked values', () => {
+    const onFilter = jest.fn()
+
+    renderWithTheme(<ExploreSidebar items={items} onFilter={onFilter} />)
+
+    userEvent.click(screen.getByLabelText(/windows/i))
+    userEvent.click(screen.getByLabelText(/linux/i))
+    userEvent.click(screen.getByLabelText(/low to high/i))
+
+    userEvent.click(screen.getByRole('button', { name: /filter/i }))
+
+    expect(onFilter).toBeCalledWith({
+      windows: true,
+      linux: true,
+      sort_by: 'low-to-high'
+    })
+  })
+
+  it('should altern between radio options', () => {
+    const onFilter = jest.fn()
+
+    renderWithTheme(<ExploreSidebar items={items} onFilter={onFilter} />)
+
+    userEvent.click(screen.getByLabelText(/low to high/i))
+    userEvent.click(screen.getByLabelText(/high to low/i))
+
+    userEvent.click(screen.getByRole('button', { name: /filter/i }))
+
+    expect(onFilter).toBeCalledWith({ sort_by: 'high-to-low' })
+  })
+
+  it('should open/close sidebar when filtering on mobile ', () => {
+    const { container } = renderWithTheme(
+      <ExploreSidebar items={items} onFilter={jest.fn} />
+    )
+
+    const variant = {
+      media: '(max-width:768px)',
+      modifier: String(css`
+        ${Overlay}
+      `)
+    }
+
+    const Element = container.firstChild
+
+    expect(Element).not.toHaveStyleRule('opacity', '1', variant)
+
+    userEvent.click(screen.getByLabelText(/open filters/))
+
+    expect(Element).toHaveStyleRule('opacity', '1', variant)
+
+    userEvent.click(screen.getByLabelText(/close filters/))
+
+    expect(Element).not.toHaveStyleRule('opacity', '1', variant)
   })
 })
