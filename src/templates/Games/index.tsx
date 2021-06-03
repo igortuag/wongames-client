e58b869bc9af8c16ea85224566928095d18f764e
Igor Tuag <igortuag@gmail.com>
2021-06-03 07:38:41 -0300

    add GamesTemplateProps

--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -1,6 +1,11 @@
+import { GameCardProps } from 'components/GameCard'
 import * as S from './styles'
 
-const GamesTemplate = () => (
+export type GamesTemplateProps = {
+  games?: GameCardProps[]
+}
+
+const GamesTemplate = ({ games = [] }: GamesTemplateProps) => (
   <S.Wrapper>
     <h1>Games</h1>
   </S.Wrapper>
