f1396e762f1dbb8227614ff74accdae3f8f0e2da
Igor Tuag <igortuag@gmail.com>
2021-06-04 07:37:54 -0300

    add ExploreSidebar component

--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -1,14 +1,21 @@
+import ExploreSidebar, { ItemProps } from 'components/ExploreSidebar'
 import { GameCardProps } from 'components/GameCard'
 import Base from 'templates/Base'
 import * as S from './styles'
 
 export type GamesTemplateProps = {
   games?: GameCardProps[]
+  filterItems: ItemProps[]
 }
 
-const GamesTemplate = ({ games = [] }: GamesTemplateProps) => (
+const GamesTemplate = ({ games = [], filterItems }: GamesTemplateProps) => (
   <Base>
-    <S.Main>Games</S.Main>
+    <S.Main>
+      <ExploreSidebar
+        items={filterItems}
+        onFilter={() => console.log('filter')}
+      />
+    </S.Main>
   </Base>
 )
 
