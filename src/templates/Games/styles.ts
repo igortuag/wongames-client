e68844d8ec85548442dc98c2fc00e38b9ecc6be9
Igor Tuag <igortuag@gmail.com>
2021-06-04 08:12:31 -0300

    add responsivity

--- src/templates/Games/styles.ts
+++ src/templates/Games/styles.ts
@@ -1,11 +1,14 @@
 import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 import { Container } from 'components/Container'
 
 export const Main = styled(Container)`
   ${({ theme }) => css`
-    display: grid;
-    grid-template-columns: 26rem 1fr;
-    gap: ${theme.grid.gutter};
+    ${media.greaterThan('medium')`
+      display: grid;
+      grid-template-columns: 26rem 1fr;
+      gap: ${theme.grid.gutter};
+    `}
   `}
 `
 
