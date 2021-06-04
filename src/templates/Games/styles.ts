68968a8a59b3b042f0819e269020df13949299ff
Igor Tuag <igortuag@gmail.com>
2021-06-04 07:35:42 -0300

    add grid to main strucuture

--- src/templates/Games/styles.ts
+++ src/templates/Games/styles.ts
@@ -1,4 +1,10 @@
+import styled, { css } from 'styled-components'
 import { Container } from 'components/Container'
-import styled from 'styled-components'
 
-export const Main = styled(Container)``
+export const Main = styled(Container)`
+  ${({ theme }) => css`
+    display: grid;
+    grid-template-columns: 26rem 1fr;
+    gap: ${theme.grid.gutter};
+  `}
+`
