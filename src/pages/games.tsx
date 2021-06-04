b482cdf1e2e0bf43231d670bdea312059956be57
Igor Tuag <igortuag@gmail.com>
2021-06-04 07:41:31 -0300

    add gamesMock to page game

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,5 +1,6 @@
 import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
 import filterItemsMock from 'components/ExploreSidebar/mock'
+import gamesMock from 'components/GameCardSlider/mock'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
@@ -8,7 +9,7 @@ export default function GamesPage(props: GamesTemplateProps) {
 export async function getServerSideProps() {
   return {
     props: {
-      games: [],
+      games: gamesMock,
       filterItems: filterItemsMock
     }
   }
