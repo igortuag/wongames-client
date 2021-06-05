b62d1d8db9b5938e80de5fff0e2e849bf742700f
Igor Tuag <igortuag@gmail.com>
2021-06-05 07:32:14 -0300

    add mocks and imports to tests

--- src/templates/Games/test.tsx
+++ src/templates/Games/test.tsx
@@ -1,6 +1,30 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helper'
+import gamesMock from 'components/GameCardSlider/mock'
+import filterItemsMock from 'components/ExploreSidebar/mock'
 
 import Games from '.'
+
+jest.mock('templates/Base', () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid="Mock Base">{children}</div>
+  }
+}))
+
+jest.mock('components/ExploreSidebar', () => ({
+  __esModule: true,
+  default: function Mock({ children }: { children: React.ReactNode }) {
+    return <div data-testid="Mock ExploreSidebar">{children}</div>
+  }
+}))
+
+jest.mock('components/GameCard', () => ({
+  __esModule: true,
+  default: function Mock() {
+    return <div data-testid="Mock GameCard" />
+  }
+}))
 
 describe('<Games />', () => {
   it('should render the heading', () => {
