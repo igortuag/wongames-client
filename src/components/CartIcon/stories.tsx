34fe977a8bed348442d6c04ab0a0fc6cbf414a33
Igor Tuag <igortuag@gmail.com>
2021-06-06 09:52:49 -0300

    add stories cartItem withItems

--- src/components/CartIcon/stories.tsx
+++ src/components/CartIcon/stories.tsx
@@ -1,5 +1,5 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import CartIcon from '.'
+import CartIcon, { CartIconProps } from '.'
 
 export default {
   title: 'CartIcon',
@@ -12,3 +12,9 @@ export default {
 } as Meta
 
 export const Default: Story = () => <CartIcon />
+
+export const withItems: Story<CartIconProps> = (args) => <CartIcon {...args} />
+
+withItems.args = {
+  quantity: 3
+}
