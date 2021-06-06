ad8345fddfc4a36f468ab161a2587b91acba88cb
Igor Tuag <igortuag@gmail.com>
2021-06-06 09:41:10 -0300

    add size to cartIcon style

--- src/components/CartIcon/styles.ts
+++ src/components/CartIcon/styles.ts
@@ -1,3 +1,8 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Wrapper = styled.main``
+export const Wrapper = styled.div`
+  ${({ theme }) => css`
+    width: 2.4rem;
+    height: 2.4rem;
+  `}
+`
