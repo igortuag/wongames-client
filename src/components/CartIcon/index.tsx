74a0f2258e403f7e0f71c3ca0523bf5822f96b04
Igor Tuag <igortuag@gmail.com>
2021-06-06 09:47:50 -0300

    add Badgeto cart

--- src/components/CartIcon/index.tsx
+++ src/components/CartIcon/index.tsx
@@ -3,6 +3,7 @@ import * as S from './styles'
 
 const CartIcon = () => (
   <S.Wrapper>
+    <S.Badge aria-label="Cart Items">12</S.Badge>
     <ShoppingCart aria-label="Shopping Cart" />
   </S.Wrapper>
 )
