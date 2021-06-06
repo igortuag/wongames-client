4c001df9a842c5b7fdfd5feb7c6cd664ecd044bd
Igor Tuag <igortuag@gmail.com>
2021-06-06 09:49:31 -0300

    add CartIconProps

--- src/components/CartIcon/index.tsx
+++ src/components/CartIcon/index.tsx
@@ -1,7 +1,12 @@
 import { ShoppingCart } from '@styled-icons/material-outlined/ShoppingCart'
+
 import * as S from './styles'
 
-const CartIcon = () => (
+export type CartIconProps = {
+  quantity?: number
+}
+
+const CartIcon = ({ quantity }: CartIconProps) => (
   <S.Wrapper>
     <S.Badge aria-label="Cart Items">12</S.Badge>
     <ShoppingCart aria-label="Shopping Cart" />
