63d9a5cead743d214ad2f2c610fdee2b45a86b5c
Igor Tuag <igortuag@gmail.com>
2021-06-06 09:41:44 -0300

    add color to icon

--- src/components/CartIcon/styles.ts
+++ src/components/CartIcon/styles.ts
@@ -2,6 +2,7 @@ import styled, { css } from 'styled-components'
 
 export const Wrapper = styled.div`
   ${({ theme }) => css`
+    color: ${theme.colors.white};
     width: 2.4rem;
     height: 2.4rem;
   `}
