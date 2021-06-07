de678d995a96e3ceb944f132c9e1da0e1c0376c9
Igor Tuag <igortuag@gmail.com>
2021-06-07 08:12:26 -0300

    add props and config

--- src/components/Dropdown/stories.tsx
+++ src/components/Dropdown/stories.tsx
@@ -1,9 +1,14 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import Dropdown from '.'
+import Dropdown, { DropdownProps } from '.'
 
 export default {
   title: 'Dropdown',
   component: Dropdown
 } as Meta
 
-export const Default: Story = () => <Dropdown />
+export const Default: Story<DropdownProps> = (args) => <Dropdown {...args} />
+
+Default.args = {
+  title: 'Click here',
+  children: 'content'
+}
