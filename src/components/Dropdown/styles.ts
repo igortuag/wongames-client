a88f42e682374e8710a63ecc95a1597d041266f6
Igor Tuag <igortuag@gmail.com>
2021-06-07 08:13:45 -0300

    add styles to Title

--- src/components/Dropdown/styles.ts
+++ src/components/Dropdown/styles.ts
@@ -1,7 +1,16 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const Wrapper = styled.div``
 
-export const Title = styled.div``
+export const Title = styled.div`
+  ${({ theme }) => css`
+    cursor: pointer;
+    color: ${theme.colors.white};
+    position: relative;
+    display: flex;
+    align-items: center;
+    padding-right: 2.4rem;
+  `}
+`
 
 export const Content = styled.div``
