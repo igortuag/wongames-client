c2e103bf9ef1cab580b5b0701a6a69af7dac8c7a
Igor Tuag <igortuag@gmail.com>
2021-06-07 08:09:46 -0300

    add DropdownProps

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,6 +1,11 @@
 import * as S from './styles'
 
-const Dropdown = () => (
+export type DropdownProps = {
+  title: React.ReactNode
+  children: React.ReactNode
+}
+
+const Dropdown = ({ title, children }: DropdownProps) => (
   <S.Wrapper>
     <h1>Dropdown</h1>
   </S.Wrapper>
