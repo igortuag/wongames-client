81499f62e6cb4474596eb08ff8b9704e33b3777a
Igor Tuag <igortuag@gmail.com>
2021-06-11 08:08:42 -0300

    add Dropdown with CartList

--- src/components/CartDropdown/index.tsx
+++ src/components/CartDropdown/index.tsx
@@ -1,3 +1,6 @@
+import CartIcon from 'components/CartIcon'
+import CartList from 'components/CartList'
+import Dropdown from 'components/Dropdown'
 import { GameItemProps } from 'components/GameItem'
 import * as S from './styles'
 
@@ -8,7 +11,9 @@ export type CartDropdownProps = {
 
 const CartDropdown = ({ items, total }: CartDropdownProps) => (
   <S.Wrapper>
-    <h1>CartDropdown</h1>
+    <Dropdown title={<CartIcon />}>
+      <CartList items={items} total={total} hasButton />
+    </Dropdown>
   </S.Wrapper>
 )
 
