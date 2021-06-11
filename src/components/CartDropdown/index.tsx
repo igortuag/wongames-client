be6ae49284a04d7cc0d735799c089207be7410f6
Igor Tuag <igortuag@gmail.com>
2021-06-11 08:09:24 -0300

    define quantityto cart icon

--- src/components/CartDropdown/index.tsx
+++ src/components/CartDropdown/index.tsx
@@ -11,7 +11,7 @@ export type CartDropdownProps = {
 
 const CartDropdown = ({ items, total }: CartDropdownProps) => (
   <S.Wrapper>
-    <Dropdown title={<CartIcon />}>
+    <Dropdown title={<CartIcon quantity={items.length} />}>
       <CartList items={items} total={total} hasButton />
     </Dropdown>
   </S.Wrapper>
