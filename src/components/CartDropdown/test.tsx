6f299986ab1e80a17408c5c1442d55c797184445
Igor Tuag <igortuag@gmail.com>
2021-06-11 08:18:05 -0300

    should render <CartIcon /> and its badge test

--- src/components/CartDropdown/test.tsx
+++ src/components/CartDropdown/test.tsx
@@ -1,13 +1,15 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helper'
+
+import items from 'components/CartList/mock'
 
 import CartDropdown from '.'
 
 describe('<CartDropdown />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<CartDropdown />)
+  it('should render <CartIcon /> and its badge', () => {
+    renderWithTheme(<CartDropdown items={items} total="R$ 300,00" />)
 
-    expect(screen.getByRole('heading', { name: /CartDropdown/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+    expect(screen.getByLabelText(/shopping cart/i)).toBeInTheDocument()
+    expect(screen.getByText(`${items.length}`)).toBeInTheDocument()
   })
 })
