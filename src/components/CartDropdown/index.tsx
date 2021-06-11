7932e867061400241650a9f4928d90d47dba5f1d
Igor Tuag <igortuag@gmail.com>
2021-06-11 08:07:09 -0300

    add CartDropdownProps

--- src/components/CartDropdown/index.tsx
+++ src/components/CartDropdown/index.tsx
@@ -1,6 +1,12 @@
+import { GameItemProps } from 'components/GameItem'
 import * as S from './styles'
 
-const CartDropdown = () => (
+export type CartDropdownProps = {
+  items: GameItemProps[]
+  total: string
+}
+
+const CartDropdown = ({ items, total }: CartDropdownProps) => (
   <S.Wrapper>
     <h1>CartDropdown</h1>
   </S.Wrapper>
