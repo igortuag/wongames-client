bed8c57f206a3774a8acc6fc14585ffa199dab68
Igor Tuag <igortuag@gmail.com>
2021-06-11 08:10:51 -0300

    add type to stories

--- src/components/CartDropdown/stories.tsx
+++ src/components/CartDropdown/stories.tsx
@@ -1,9 +1,11 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import CartDropdown from '.'
+import CartDropdown, { CartDropdownProps } from '.'
 
 export default {
   title: 'CartDropdown',
   component: CartDropdown
 } as Meta
 
-export const Default: Story = () => <CartDropdown />
+export const Default: Story<CartDropdownProps> = (args) => (
+  <CartDropdown {...args} />
+)
