6c33c8e7b822075f4ff6d6e28f5efc21cacf097f
Igor Tuag <igortuag@gmail.com>
2021-06-11 08:20:32 -0300

    should render Dropdown content with cart items and total

--- src/components/CartDropdown/test.tsx
+++ src/components/CartDropdown/test.tsx
@@ -12,4 +12,11 @@ describe('<CartDropdown />', () => {
     expect(screen.getByLabelText(/shopping cart/i)).toBeInTheDocument()
     expect(screen.getByText(`${items.length}`)).toBeInTheDocument()
   })
+
+  it('should render Dropdown content with cart items and total', () => {
+    renderWithTheme(<CartDropdown items={items} total="R$ 300,00" />)
+
+    expect(screen.getByText('R$ 300,00')).toBeInTheDocument()
+    expect(screen.getByText(`${items[0].title}`)).toBeInTheDocument()
+  })
 })
