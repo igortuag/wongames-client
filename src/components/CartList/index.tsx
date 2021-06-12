d4e4b6f9d666d666dfa3898629b8777f9ceb724f
Igor Tuag <igortuag@gmail.com>
2021-06-12 08:17:09 -0300

    change items and total to optional

--- src/components/CartList/index.tsx
+++ src/components/CartList/index.tsx
@@ -4,12 +4,12 @@ import GameItem, { GameItemProps } from 'components/GameItem'
 import * as S from './styles'
 
 export type CartListProps = {
-  items: GameItemProps[]
-  total: string
+  items?: GameItemProps[]
+  total?: string
   hasButton?: boolean
 }
 
-const CartList = ({ items, total, hasButton = false }: CartListProps) => (
+const CartList = ({ items = [], total, hasButton = false }: CartListProps) => (
   <S.Wrapper>
     {items.map((item) => (
       <GameItem key={item.title} {...item} />
