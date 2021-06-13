3f0fbcdf2ac65ab411667461321281c4069c5a6a
Igor Tuag <igortuag@gmail.com>
2021-06-13 17:52:39 -0300

    generate user dropdown

---
+++ src/components/UserDropdown/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles'
+
+const UserDropdown = () => (
+  <S.Wrapper>
+    <h1>UserDropdown</h1>
+  </S.Wrapper>
+)
+
+export default UserDropdown
---
+++ src/components/UserDropdown/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import UserDropdown from '.'
+
+export default {
+  title: 'UserDropdown',
+  component: UserDropdown
+} as Meta
+
+export const Default: Story = () => <UserDropdown />
