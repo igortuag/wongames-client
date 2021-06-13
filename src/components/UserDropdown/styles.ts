958095fe541a22b37a4336c9c721465497a33664
Igor Tuag <igortuag@gmail.com>
2021-06-13 18:14:48 -0300

    add styles to nav

--- src/components/UserDropdown/styles.ts
+++ src/components/UserDropdown/styles.ts
@@ -1,5 +1,15 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Nav = styled.nav``
+export const Nav = styled.nav`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    width: 26rem;
+
+    a:not(:last-child) {
+      border-bottom: 0.1rem solid ${theme.colors.lightGray};
+    }
+  `}
+`
 
 export const Link = styled.a``
