bbdee63297bc4d8ef27d0de229c720e79a379a92
Igor Tuag <igortuag@gmail.com>
2021-06-14 08:10:00 -0300

    should render the menu test

--- src/components/UserDropdown/test.tsx
+++ src/components/UserDropdown/test.tsx
@@ -1,4 +1,5 @@
 import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 
 import { renderWithTheme } from 'utils/tests/helper'
 
@@ -9,5 +10,14 @@ describe('<UserDropdown />', () => {
     renderWithTheme(<UserDropdown username="Igor Tuag" />)
 
     expect(screen.getByText(/Igor Tuag/i)).toBeInTheDocument()
+  })
+
+  it('should render the menu', () => {
+    renderWithTheme(<UserDropdown username="Igor Tuag" />)
+
+    userEvent.click(screen.getByText(/Igor Tuag/i))
+
+    expect(screen.getByRole('link', { name: /wishlist/i })).toBeInTheDocument()
+    expect(screen.getByRole('link', { name: /sign out/i })).toBeInTheDocument()
   })
 })
