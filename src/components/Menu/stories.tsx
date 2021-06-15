855e501323e96f417e7c3ad607383f266eef0860
Igor Tuag <igortuag@gmail.com>
2021-06-15 08:01:07 -0300

    set parameters by default

--- src/components/Menu/stories.tsx
+++ src/components/Menu/stories.tsx
@@ -3,17 +3,16 @@ import Menu, { MenuProps } from '.'
 
 export default {
   title: 'Menu',
-  component: Menu
+  component: Menu,
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
 } as Meta
 
 export const Default: Story<MenuProps> = (args) => <Menu {...args} />
-
-Default.parameters = {
-  layout: 'fullscreen',
-  backgrounds: {
-    default: 'won-dark'
-  }
-}
 
 export const Logged: Story<MenuProps> = (args) => <Menu {...args} />
 
