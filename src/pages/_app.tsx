a9441007df833d0339c6d1d82683db51d988e229
Igor Tuag <igortuag@gmail.com>
2021-06-19 08:14:00 -0300

    show sample data

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
-import { ApolloProvider } from '@apollo/client'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
-import { ThemeProvider } from 'styled-components'
-import { ApolloClient, gql, InMemoryCache } from '@apollo/client'
+import { ApolloClient, InMemoryCache } from '@apollo/client'
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
+import { ThemeProvider } from 'styled-components'
+import { ApolloProvider } from '@apollo/client'
 
 function App({ Component, pageProps }: AppProps) {
   const client = new ApolloClient({
