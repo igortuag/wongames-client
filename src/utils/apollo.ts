3a6a4ae364274dcae255a973b5d5c06ce773958d
Igor Tuag <igortuag@gmail.com>
2021-06-20 07:45:43 -0300

    add ssrMode

--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -8,6 +8,7 @@ let apolloClient: ApolloClient<NormalizedCacheObject>
 
 function createApolloClient() {
   return new ApolloClient({
+    ssrMode: typeof window === 'undefined',
     uri: 'http://localhost:1337/graphql',
     cache: new InMemoryCache()
   })
