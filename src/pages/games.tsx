6571dcf2acba575cf85453778dac73d2e9057683
Igor Tuag <igortuag@gmail.com>
2021-06-22 08:17:01 -0300

    add QueryGames

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -2,6 +2,7 @@ import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
 import filterItemsMock from 'components/ExploreSidebar/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import { initializeApollo } from 'utils/apollo'
+import { gql } from '@apollo/client'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
@@ -9,6 +10,24 @@ export default function GamesPage(props: GamesTemplateProps) {
 
 export async function getStaticProps() {
   const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query({
+    query: gql`
+      query QueryGames {
+        games {
+          name
+          slug
+          cover {
+            url
+          }
+          developers {
+            name
+          }
+          price
+        }
+      }
+    `
+  })
 
   return {
     props: {
