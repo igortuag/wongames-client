5e0f86cafd26b79e7057c641943bbf4aedf4732f
Igor Tuag <igortuag@gmail.com>
2021-06-22 08:16:20 -0300

    add apolloClient

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,12 +1,15 @@
 import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
 import filterItemsMock from 'components/ExploreSidebar/mock'
 import gamesMock from 'components/GameCardSlider/mock'
+import { initializeApollo } from 'utils/apollo'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
 }
 
 export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
   return {
     props: {
       revalidate: 60,
