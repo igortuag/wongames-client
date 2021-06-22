99c76ebaaaa2de584d9236aae1c312e9606ed296
Igor Tuag <igortuag@gmail.com>
2021-06-22 08:10:25 -0300

    remvoe test data example

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,8 @@
 import Home, { HomeTemplateProps } from 'templates/Home'
-import { gql, useQuery } from '@apollo/client'
 
 import bannersMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
-import { initializeApollo } from 'utils/apollo'
-
-const GET_GAMES = gql`
-  query getGames {
-    games {
-      name
-    }
-  }
-`
 
 export default function Index(props: HomeTemplateProps) {
   if (props.data) return <p>{JSON.stringify(props.data, null, 2)}</p>
@@ -21,14 +11,8 @@ export default function Index(props: HomeTemplateProps) {
 }
 
 export async function getServerSideProps() {
-  const apolloClient = initializeApollo()
-
-  const data = await apolloClient.query({ query: GET_GAMES })
-
   return {
     props: {
-      data: data,
-      initialApolloState: apolloClient.cache.extract(),
       banners: bannersMock,
       newGames: gamesMock,
       mostPopularHighlight: highlightMock,
