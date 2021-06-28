6f27c47bdf39a32f2f8c9e9fb46057007712b144
Igor Tuag <igortuag@gmail.com>
2021-06-28 08:23:51 -0300

    add QUERY_GAME_BY_SLUG

--- src/graphql/queries/games.ts
+++ src/graphql/queries/games.ts
@@ -15,3 +15,33 @@ export const QUERY_GAMES = gql`
     }
   }
 `
+
+export const QUERY_GAME_BY_SLUG = gql`
+  # Write your query or mutation here
+  query QueryGameBySlug($slug: String!) {
+    games(where: { slug: $slug }) {
+      name
+      short_description
+      description
+      price
+      rating
+      release_date
+      gallery {
+        url
+        label: alternativeText
+      }
+      cover {
+        src: url
+      }
+      developers {
+        name
+      }
+      publisher {
+        name
+      }
+      categories {
+        name
+      }
+    }
+  }
+`
