a7b11c1ad2d647d5242a6b7af4a57c8de21cc8a6
Igor Tuag <igortuag@gmail.com>
2021-06-30 07:39:36 -0300

    remove types from jest

--- jest.config.js
+++ jest.config.js
@@ -7,7 +7,8 @@ module.exports = {
     '!src/**/stories.tsx',
     '!src/pages/**/*.tsx',
     '!src/styles/**/*.ts',
-    'src/utils/apollo.ts'
+    '!src/utils/apollo.ts',
+    '!src/types/**/*.d.ts'
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
