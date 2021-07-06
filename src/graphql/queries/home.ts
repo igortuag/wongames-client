6fcd7f1feb31ee1a2b17f6ad6eeb1ccce46813db
Igor Tuag <igortuag@gmail.com>
2021-07-06 07:16:29 -0300

    add banner fragment

--- src/graphql/queries/home.ts
+++ src/graphql/queries/home.ts
@@ -1,22 +1,26 @@
 import { gql } from '@apollo/client'
 
 export const QUERY_HOME = gql`
+  fragment BannerFragment on Banner {
+    image {
+      url
+    }
+    title
+    subtitle
+    button {
+      label
+      link
+    }
+    ribbon {
+      text
+      color
+      size
+    }
+  }
+
   query QueryHome {
     banners {
-      image {
-        url
-      }
-      title
-      subtitle
-      button {
-        label
-        link
-      }
-      ribbon {
-        text
-        color
-        size
-      }
+      ...BannerFragment
     }
   }
 `
