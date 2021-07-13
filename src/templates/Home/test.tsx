7ba2183fc986f6dd9d4ac228fd5b4bf952057f10
Igor Tuag <igortuag@gmail.com>
2021-07-13 08:12:11 -0300

    adjust test for just four show cases

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -42,7 +42,7 @@ describe('<Home />', () => {
   it('should render banner and showcases', () => {
     renderWithTheme(<Home {...props} />)
 
-    expect(screen.getAllByTestId('Mock Showcase')).toHaveLength(5)
+    expect(screen.getAllByTestId('Mock Showcase')).toHaveLength(4)
     expect(screen.getByTestId('Mock Banner Slider')).toBeInTheDocument()
   })
 })
