eccab8d9ea577b7b607cffe84af512118fbff294
Igor Tuag <igortuag@gmail.com>
2021-07-18 08:50:02 -0300

    add variable date

--- src/graphql/queries/home.ts
+++ src/graphql/queries/home.ts
@@ -8,13 +8,13 @@ export const QUERY_HOME = gql`
   ${GameFragment}
   ${HighlightFragment}
 
-  query QueryHome {
+  query QueryHome($date: Date!) {
     banners {
       ...BannerFragment
     }
 
     newGames: games(
-      where: { release_date_lte: "2021-01-27" }
+      where: { release_date_lte: $date }
       sort: "release_date:desc"
       limit: 8
     ) {
@@ -22,7 +22,7 @@ export const QUERY_HOME = gql`
     }
 
     upcomingGames: games(
-      where: { release_date_gt: "2021-01-27" }
+      where: { release_date_gt: $date }
       sort: "release_date:asc"
       limit: 8
     ) {
