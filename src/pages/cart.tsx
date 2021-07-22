60ebfde7857729aa8a0f9d12c21f4ca03b51c8e2
Igor Tuag <igortuag@gmail.com>
2021-07-22 08:06:51 -0300

    add data from api on page

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,12 +1,12 @@
 import Cart, { CartProps } from 'templates/Cart'
 
 import itemsMock from 'components/CartList/mock'
-import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
 import cardsMock from 'components/PaymentOptions/mock'
 import { initializeApollo } from 'utils/apollo'
 import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
 import { QueryRecommended } from 'graphql/generated/QueryRecommended'
+import { gamesMapper } from 'utils/mappers'
 
 export default function CartPage(props: CartProps) {
   return <Cart {...props} />
@@ -24,7 +24,8 @@ export async function getServerSideProps() {
       items: itemsMock,
       total: '$ 430,00',
       cards: cardsMock,
-      recommendedGames: gamesMock.slice(0, 5),
+      recommendedTitle: data.recommended?.section?.highlight?.title,
+      recommendedGames: gamesMapper(data.recommended?.section?.games),
       recommendedHighlights: highlightMock
     }
   }
