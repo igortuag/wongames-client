bde0ded74503f7f19d9cf76ccd93cc04b9a94b37
Igor Tuag <igortuag@gmail.com>
2021-07-27 08:04:18 -0300

    remove non used loading

--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -15,12 +15,9 @@ export type GamesTemplateProps = {
 }
 
 const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
-  const { data, loading } = useQuery<QueryGames, QueryGamesVariables>(
-    QUERY_GAMES,
-    {
-      variables: { limit: 15 }
-    }
-  )
+  const { data } = useQuery<QueryGames, QueryGamesVariables>(QUERY_GAMES, {
+    variables: { limit: 15 }
+  })
 
   const handleFilter = () => {
     return
@@ -34,29 +31,25 @@ const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
     <Base>
       <S.Main>
         <ExploreSidebar items={filterItems} onFilter={handleFilter} />
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-          <section>
-            <Grid>
-              {data?.games.map((game) => (
-                <GameCard
-                  key={game.slug}
-                  title={game.name}
-                  slug={game.slug}
-                  developer={game.developers[0].name}
-                  img={`http://localhost:1337${game.cover!.url}`}
-                  price={game.price}
-                />
-              ))}
-            </Grid>
+        <section>
+          <Grid>
+            {data?.games.map((game) => (
+              <GameCard
+                key={game.slug}
+                title={game.name}
+                slug={game.slug}
+                developer={game.developers[0].name}
+                img={`http://localhost:1337${game.cover!.url}`}
+                price={game.price}
+              />
+            ))}
+          </Grid>
 
-            <S.ShowMore role="button" onClick={handleShowMore}>
-              <p>Show More</p>
-              <ArrowDown size={35} />
-            </S.ShowMore>
-          </section>
-        )}
+          <S.ShowMore role="button" onClick={handleShowMore}>
+            <p>Show More</p>
+            <ArrowDown size={35} />
+          </S.ShowMore>
+        </section>
       </S.Main>
     </Base>
   )
