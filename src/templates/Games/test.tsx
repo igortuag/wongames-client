69ae3729150ee57f3e32f6a056b2bf9dc99085ca
Igor Tuag <igortuag@gmail.com>
2021-08-01 07:58:03 -0300

    add test should render more games when sho more is cliecked

--- src/templates/Games/test.tsx
+++ src/templates/Games/test.tsx
@@ -3,9 +3,10 @@ import { MockedProvider } from '@apollo/client/testing'
 
 import { renderWithTheme } from 'utils/tests/helper'
 import filterItemsMock from 'components/ExploreSidebar/mock'
-import { gamesMock } from './mocks'
+import { fetchMoreMock, gamesMock } from './mocks'
 
 import Games from '.'
+import userEvent from '@testing-library/user-event'
 
 jest.mock('templates/Base', () => ({
   __esModule: true,
@@ -36,10 +37,7 @@ describe('<Games />', () => {
 describe('<Games />', () => {
   it('should render sections', async () => {
     renderWithTheme(
-      <MockedProvider
-        mocks={[gamesMock]}
-        addTypename={false}
-      >
+      <MockedProvider mocks={[gamesMock]} addTypename={false}>
         <Games filterItems={filterItemsMock} />
       </MockedProvider>
     )
@@ -47,10 +45,24 @@ describe('<Games />', () => {
     expect(screen.getByText(/loading.../i)).toBeInTheDocument()
 
     expect(await screen.findByTestId('Mock ExploreSidebar')).toBeInTheDocument()
-    expect(await screen.findByText(/sample/i)).toBeInTheDocument()
+    expect(await screen.findByText(/Sample Game/i)).toBeInTheDocument()
 
     expect(
       await screen.findByRole('button', { name: /show more/i })
     ).toBeInTheDocument()
+  })
+
+  it('should render more games when sho more is cliecked', async () => {
+    renderWithTheme(
+      <MockedProvider mocks={[gamesMock, fetchMoreMock]} addTypename={false}>
+        <Games filterItems={filterItemsMock} />
+      </MockedProvider>
+    )
+
+    expect(await screen.findByText(/Sample Game/i)).toBeInTheDocument()
+
+    userEvent.click(await screen.findByRole('button', { name: /show more/i }))
+
+    expect(await screen.findByText(/Fetch More Game/i)).toBeInTheDocument()
   })
 })
