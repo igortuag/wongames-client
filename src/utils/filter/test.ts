39211ef03868d50ad914ce20050a1048830bda1a
Igor Tuag <igortuag@gmail.com>
2021-08-04 08:45:03 -0300

    add test to queryString to where format

--- src/utils/filter/test.ts
+++ src/utils/filter/test.ts
@@ -1,4 +1,4 @@
-import { parserQueryStringToWhere } from '.'
+import { parseQueryStringToWhere } from '.'
 
 const filterItems = [
   { name: 'price_lte', type: 'radio' },
@@ -6,3 +6,13 @@ const filterItems = [
   { name: 'developers', type: 'checkbox' },
   { name: 'sort', type: 'radio' }
 ]
+
+const queryString = {
+  price_lte: 1000
+}
+
+describe('parserQueryStringToWhere', () => {
+  it('shoud parse queryString to where format', () => {
+    const parseQuery = parseQueryStringToWhere({ queryString, filterItems })
+  })
+})
