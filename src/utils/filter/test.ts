a91dfe064054767a8f5377466b07b1bf81319dce
Igor Tuag <igortuag@gmail.com>
2021-08-04 08:42:05 -0300

    add filterItems to test filter

---
+++ src/utils/filter/test.ts
@@ -0,0 +1,8 @@
+import { parserQueryStringToWhere } from '.'
+
+const filterItems = [
+  { name: 'price_lte', type: 'radio' },
+  { name: 'platforms', type: 'checkbox' },
+  { name: 'developers', type: 'checkbox' },
+  { name: 'sort', type: 'radio' }
+]
