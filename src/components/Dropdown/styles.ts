fa236f35b5877c894a64e63caee92dcc03b331ce
Tuag <igortuag@gmail.com>
2021-08-19 07:03:32 -0300

    add overlay to dropdown

--- src/components/Dropdown/styles.ts
+++ src/components/Dropdown/styles.ts
@@ -32,7 +32,17 @@ export const Content = styled.div`
   `}
 `
 
-export const Overlay = styled.div``
+export const Overlay = styled.div`
+  ${({ theme }) => css`
+    background: rgba(0, 0, 0, 0.5);
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: ${theme.layers.overlay};
+  `}
+`
 
 type WrapperProps = {
   isOpen?: boolean
