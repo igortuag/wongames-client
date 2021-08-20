12901cc412a629d1e2b6bcbbd7c69d8ba20fe96a
Tuag <igortuag@gmail.com>
2021-08-20 07:25:08 -0300

    should return the item from localStorage test

--- src/utils/localStorage/test.ts
+++ src/utils/localStorage/test.ts
@@ -1,5 +1,11 @@
 import { getStorageItem, setStorageItem } from '.'
 
-describe('getStorageItem', () => {})
+describe('getStorageItem', () => {]
+ it('should return the item from localStorage', () => {
+  window.localStorage.setItem('WONGAMES_cartItems', JSON.stringify(['1', '2']))
+
+  expect(getStorageItem('cartItems')).toEqual(['1', '2'])
+ })
+})
 
 describe('setStorageItem', () => {})
