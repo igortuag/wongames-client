9b182726386a31442f98fe884691145f7f93acde
Tuag <igortuag@gmail.com>
2021-08-20 07:27:51 -0300

    add methods to getStorageItem

--- src/utils/localStorage/index.ts
+++ src/utils/localStorage/index.ts
@@ -1,3 +1,10 @@
-export function getStorageItem() {}
+const APP_KEY = 'WONGAMES'
+
+export function getStorageItem(key: string) {
+  if (typeof window !== 'undefined') return
+
+  const data = window.localStorage.getItem(`${APP_KEY}_${key}`)
+  return JSON.parse(data!)
+}
 
 export function setStorageItem() {}
