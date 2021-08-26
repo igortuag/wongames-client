207a594a3f3ac06375fd1462fe9b7295d4cd703b
Tuag <igortuag@gmail.com>
2021-08-26 07:29:28 -0300

    should return true/false if the item is already in the cart test

--- src/hooks/use-cart/test.tsx
+++ src/hooks/use-cart/test.tsx
@@ -7,6 +7,9 @@ import { cartItems, gamesMock } from './mock'
 
 describe('useCart', () => {
   it('should return items and its info there are any in the cart', async () => {
+    beforeEach(() => {
+      window.localStorage.clear()
+    })
     const wrapper = ({ children }: CartProviderProps) => (
       <MockedProvider mocks={[gamesMock]}>
         <CartProvider>{children}</CartProvider>
@@ -24,5 +27,26 @@ describe('useCart', () => {
     expect(result.current.items).toStrictEqual(cartItems)
     expect(result.current.quantity).toBe(2)
     expect(result.current.total).toBe('$21.00')
+  })
+
+  it('should return true/false if the item is already in the cart', () => {
+    beforeEach(() => {
+      window.localStorage.clear()
+    })
+
+    const wrapper = ({ children }: CartProviderProps) => (
+      <MockedProvider mocks={[gamesMock]}>
+        <CartProvider>{children}</CartProvider>
+      </MockedProvider>
+    )
+
+    setStorageItem('cartItem', ['1'])
+
+    const { result } = renderHook(() => useCart(), {
+      wrapper
+    })
+
+    expect(result.current.isInCart('1')).toBe(true)
+    expect(result.current.isInCart('2')).toBe(false)
   })
 })
