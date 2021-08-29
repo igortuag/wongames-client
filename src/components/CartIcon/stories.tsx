654c1b9aed69d385a0a9c603100a6525db5d0b8b
Tuag <igortuag@gmail.com>
2021-08-29 06:47:30 -0300

    remove non used cart props

--- src/components/CartIcon/stories.tsx
+++ src/components/CartIcon/stories.tsx
@@ -1,5 +1,5 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import CartIcon, { CartIconProps } from '.'
+import CartIcon from '.'
 
 export default {
   title: 'CartIcon',
@@ -13,7 +13,7 @@ export default {
 
 export const Default: Story = () => <CartIcon />
 
-export const withItems: Story<CartIconProps> = (args) => <CartIcon {...args} />
+export const withItems: Story = (args) => <CartIcon {...args} />
 
 withItems.args = {
   quantity: 3
