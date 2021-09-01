e177990f2251002a2281b1609a548199f7f4be0a
Tuag <igortuag@gmail.com>
2021-09-01 08:03:26 -0300

    should render button to remove and call the method if clicked test

--- src/components/CartButton/test.tsx
+++ src/components/CartButton/test.tsx
@@ -14,12 +14,26 @@ describe('<CartButton />', () => {
 
     render(<CartButton id="1" />, { cartProviderProps })
 
-    const button = screen.getByLabelText(/add ti cart/i)
-
+    const button = screen.getByLabelText(/add to cart/i)
     expect(button).toBeInTheDocument()
 
     userEvent.click(button)
+    expect(cartProviderProps.addToCart).toHaveBeenCalledWith('1')
+  })
 
+  it('should render button to remove and call the method if clicked', () => {
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      isInCart: () => true,
+      removeFromCart: jest.fn()
+    }
+
+    render(<CartButton id="1" />, { cartProviderProps })
+
+    const button = screen.getByLabelText(/remove from cart/i)
+    expect(button).toBeInTheDocument()
+
+    userEvent.click(button)
     expect(cartProviderProps.addToCart).toHaveBeenCalledWith('1')
   })
 })
