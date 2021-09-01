a583c5360ea93e41749e965b61458d9d4218cd4c
Tuag <igortuag@gmail.com>
2021-09-01 07:54:43 -0300

    add useCart

--- src/components/CartButton/index.tsx
+++ src/components/CartButton/index.tsx
@@ -1,12 +1,15 @@
 import { AddShoppingCart } from '@styled-icons/material-outlined'
 
 import Button from 'components/Button'
+import { useCart } from 'hooks/use-cart'
 
 type CartButtonProps = {
   id: string
 }
 
 const CartButton = ({ id }: CartButtonProps) => {
+  const { isInCart, addToCart, removeFromCart } = useCart()
+
   return <Button icon={<AddShoppingCart />} size="small" />
 }
 
