0246171d7c82cfcae74b486748d9ee70c5ed4c4b
Tuag <igortuag@gmail.com>
2021-09-01 07:44:13 -0300

    add structure to cart button

---
+++ src/components/CartButton/index.tsx
@@ -0,0 +1,8 @@
+import Button from 'components/Button'
+import { AddShoppingCart } from '@styled-icons/material-outlined'
+
+const CartButton = () => {
+  return <Button icon={<AddShoppingCart />} size="small" />
+}
+
+export default CartButton
