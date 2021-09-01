5ecc3cf14c644043d94e4f71f7ce2916d66a48e6
Tuag <igortuag@gmail.com>
2021-09-01 07:59:01 -0300

    should render button to add and call the method if clicked test

---
+++ src/components/CartButton/test.tsx
@@ -0,0 +1,20 @@
+import { screen, render } from 'utils/test-utils'
+import { CartContextDefaultValues } from 'hooks/use-cart'
+
+import CartButton from '.'
+
+describe('<CartButton />', () => {
+  it('should render button to add and call the method if clicked', () => {
+    const cartProviderProps = {
+      ...CartContextDefaultValues,
+      isInCart: () => false,
+      addToCart: jest.fn()
+    }
+
+    render(<CartButton id="1" />, { cartProviderProps })
+
+    const button = screen.getByLabelText(/add ti cart/i)
+
+    expect(button).toBeInTheDocument()
+  })
+})
