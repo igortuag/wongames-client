b5e5014d1f70e932d3282f1315d14010620b1350
Tuag <igortuag@gmail.com>
2021-09-02 07:05:02 -0300

    add IsInCart stories

--- src/components/GameCard/stories.tsx
+++ src/components/GameCard/stories.tsx
@@ -1,4 +1,5 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
+import { CartContextData } from 'hooks/use-cart'
 
 import GameCard, { GameCardProps } from '.'
 
@@ -44,3 +45,17 @@ export const Default: Story<GameCardProps> = (args) => (
     <GameCard {...args} />
   </div>
 )
+
+export const IsInCart: Story<GameCardProps & CartContextData> = (args) => (
+  <div
+    style={{
+      width: '30rem'
+    }}
+  >
+    <GameCard {...args} />
+  </div>
+)
+
+IsInCart.args = {
+  isInCart: () => true
+}
