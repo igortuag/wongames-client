0be2f5469f576d3e46aac9481d72a0f36a5251ce
Tuag <igortuag@gmail.com>
2021-09-03 06:56:04 -0300

    replace button to add for cart button

--- src/components/GameInfo/index.tsx
+++ src/components/GameInfo/index.tsx
@@ -1,9 +1,7 @@
-import {
-  AddShoppingCart,
-  FavoriteBorder
-} from '@styled-icons/material-outlined'
+import { FavoriteBorder } from '@styled-icons/material-outlined'
 
 import Button from 'components/Button'
+import CartButton from 'components/CartButton'
 import Heading from 'components/Heading'
 import Ribbon from 'components/Ribbon'
 import formatPrice from 'utils/format-price'
@@ -28,9 +26,7 @@ const GameInfo = ({ id, title, description, price }: GameInfoProps) => (
     <S.Description>{description}</S.Description>
 
     <S.ButtonsWrapper>
-      <Button icon={<AddShoppingCart />} size="large">
-        Add to cart
-      </Button>
+      <CartButton id={id} />
       <Button icon={<FavoriteBorder />} size="large" minimal>
         Wishlist
       </Button>
