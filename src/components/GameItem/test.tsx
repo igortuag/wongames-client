0e9c280bfd7d1747ed2437f318774caad7f2cc22
Tuag <igortuag@gmail.com>
2021-09-07 07:18:07 -0300

    add id props

--- src/components/GameItem/test.tsx
+++ src/components/GameItem/test.tsx
@@ -4,6 +4,7 @@ import { renderWithTheme } from 'utils/tests/helper'
 import GameItem from '.'
 
 const props = {
+  id: '1',
   img: 'https://source.unsplash.com/user/willianjusten/151x70',
   title: 'Red Dead Redemption 2',
   price: 'R$ 215,00'
