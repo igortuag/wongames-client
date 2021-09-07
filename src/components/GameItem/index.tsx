93b481fcd75308f3fc801c648c4d376cb8b88a7d
Tuag <igortuag@gmail.com>
2021-09-07 07:13:28 -0300

    add use cart

--- src/components/GameItem/index.tsx
+++ src/components/GameItem/index.tsx
@@ -1,5 +1,6 @@
 import * as S from './styles'
 import { Download } from '@styled-icons/boxicons-solid/Download'
+import { useCart } from 'hooks/use-cart'
 
 export type PaymentInfoProps = {
   number: string
@@ -22,39 +23,43 @@ const GameItem = ({
   price,
   downloadLink,
   paymentInfo
-}: GameItemProps) => (
-  <S.Wrapper>
-    <S.GameContent>
-      <S.ImageBox>
-        <img src={img} alt={title} />
-      </S.ImageBox>
-      <S.Content>
-        <S.Title>
-          {title}
-          {!!downloadLink && (
-            <S.DownloadLink
-              href={downloadLink}
-              target="_blank"
-              aria-label={`Get ${title} here`}
-            >
-              <Download size={22} />
-            </S.DownloadLink>
-          )}
-        </S.Title>
-        <S.Price>{price}</S.Price>
-      </S.Content>
-    </S.GameContent>
+}: GameItemProps) => {
+  const { isInCart, removeFromCart } = useCart()
 
-    {!!paymentInfo && (
-      <S.PaymentContent>
-        <p>{paymentInfo.purchaseDate}</p>
-        <S.CardInfo>
-          <span>{paymentInfo.number}</span>
-          <img src={paymentInfo.img} alt={paymentInfo.flag} />
-        </S.CardInfo>
-      </S.PaymentContent>
-    )}
-  </S.Wrapper>
-)
+  return (
+    <S.Wrapper>
+      <S.GameContent>
+        <S.ImageBox>
+          <img src={img} alt={title} />
+        </S.ImageBox>
+        <S.Content>
+          <S.Title>
+            {title}
+            {!!downloadLink && (
+              <S.DownloadLink
+                href={downloadLink}
+                target="_blank"
+                aria-label={`Get ${title} here`}
+              >
+                <Download size={22} />
+              </S.DownloadLink>
+            )}
+          </S.Title>
+          <S.Price>{price}</S.Price>
+        </S.Content>
+      </S.GameContent>
+
+      {!!paymentInfo && (
+        <S.PaymentContent>
+          <p>{paymentInfo.purchaseDate}</p>
+          <S.CardInfo>
+            <span>{paymentInfo.number}</span>
+            <img src={paymentInfo.img} alt={paymentInfo.flag} />
+          </S.CardInfo>
+        </S.PaymentContent>
+      )}
+    </S.Wrapper>
+  )
+}
 
 export default GameItem
