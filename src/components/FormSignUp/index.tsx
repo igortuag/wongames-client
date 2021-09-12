75738b5eafaa80bd0c00259f16405deb2a03d2a8
Tuag <igortuag@gmail.com>
2021-09-12 07:35:16 -0300

    add initial state to form sign up

--- src/components/FormSignUp/index.tsx
+++ src/components/FormSignUp/index.tsx
@@ -1,50 +1,60 @@
+import { useState } from 'react'
 import Link from 'next/link'
 import { AccountCircle, Email, Lock } from '@styled-icons/material-outlined'
 
 import { FormWrapper, FormLink } from 'components/Form'
 import Button from 'components/Button'
 import TextField from 'components/TextField'
+import { UsersPermissionsRegisterInput } from 'graphql/generated/globalTypes'
 
-const FormSignUp = () => (
-  <FormWrapper>
-    <form>
-      <TextField
-        name="name"
-        placeholder="Name"
-        type="name"
-        icon={<AccountCircle />}
-      />
-      <TextField
-        name="email"
-        placeholder="Email"
-        type="email"
-        icon={<Email />}
-      />
-      <TextField
-        name="password"
-        placeholder="Password"
-        type="password"
-        icon={<Lock />}
-      />
-      <TextField
-        name="confirm-password"
-        placeholder="Confirm password"
-        type="password"
-        icon={<Lock />}
-      />
+const FormSignUp = () => {
+  const [values, setValues] = useState<UsersPermissionsRegisterInput>({
+    username: '',
+    email: '',
+    password: ''
+  })
 
-      <Button size="large" fullWidth>
-        Sign up now
-      </Button>
+  return (
+    <FormWrapper>
+      <form>
+        <TextField
+          name="name"
+          placeholder="Name"
+          type="name"
+          icon={<AccountCircle />}
+        />
+        <TextField
+          name="email"
+          placeholder="Email"
+          type="email"
+          icon={<Email />}
+        />
+        <TextField
+          name="password"
+          placeholder="Password"
+          type="password"
+          icon={<Lock />}
+        />
+        <TextField
+          name="confirm-password"
+          placeholder="Confirm password"
+          type="password"
+          icon={<Lock />}
+        />
 
-      <FormLink>
-        Already have an account?{' '}
-        <Link href="/sign-in">
-          <a>Sign in</a>
-        </Link>
-      </FormLink>
-    </form>
-  </FormWrapper>
-)
+        <Button size="large" fullWidth>
+          Sign up now
+        </Button>
+
+        <FormLink>
+          Already have an account?{' '}
+          <Link href="/sign-in">
+            <a>Sign in</a>
+          </Link>
+        </FormLink>
+      </form>
+    </FormWrapper>
+  )
+}
 
 export default FormSignUp
