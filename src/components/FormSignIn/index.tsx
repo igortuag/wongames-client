b2fac9c92bef17ab9d29c78a52f114d7f72a990d
Tuag <igortuag@gmail.com>
2021-09-16 07:12:22 -0300

    add methods to change form and submit

--- src/components/FormSignIn/index.tsx
+++ src/components/FormSignIn/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Link from 'next/link'
 import { Email, Lock } from '@styled-icons/material-outlined'
 import Button from 'components/Button'
@@ -5,36 +6,66 @@ import Button from 'components/Button'
 import { FormWrapper, FormLink } from 'components/Form'
 import TextField from 'components/TextField'
 import * as S from './styles'
+import { useMutation } from '@apollo/client'
 
-const FormSignIn = () => (
-  <FormWrapper>
-    <form>
-      <TextField
-        name="email"
-        placeholder="Email"
-        type="email"
-        icon={<Email />}
-      />
-      <TextField
-        name="password"
-        placeholder="Password"
-        type="password"
-        icon={<Lock />}
-      />
-      <S.ForgotPassword href="#">Forgot your password?</S.ForgotPassword>
+const FormSignIn = () => {
+  const [values, setValues] = useState<>({
+    email: '',
+    password: ''
+  })
 
-      <Button size="large" fullWidth>
-        Sign in now
-      </Button>
+  const [createUser] = useMutation()
 
-      <FormLink>
-        Don’t have an account?{' '}
-        <Link href="/sign-up">
-          <a>Sign up</a>
-        </Link>
-      </FormLink>
-    </form>
-  </FormWrapper>
-)
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault()
+
+    createUser({
+      variables: {
+        input: {
+          username: values.username,
+          email: values.email,
+          password: values.password
+        }
+      }
+    })
+  }
+
+  const handleInput = (field: string, value: string) => {
+    setValues((s) => ({ ...s, [field]: value }))
+  }
+
+  return (
+    <FormWrapper>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          name="email"
+          placeholder="Email"
+          type="email"
+          onInputChange={(v) => handleInput('email', v)}
+          icon={<Email />}
+        />
+        <TextField
+          name="password"
+          placeholder="Password"
+          type="password"
+          onInputChange={(v) => handleInput('password', v)}
+          icon={<Lock />}
+        />
+        <S.ForgotPassword href="#">Forgot your password?</S.ForgotPassword>
+
+        <Button type="submit" size="large" fullWidth>
+          Sign in now
+        </Button>
+
+        <FormLink>
+          Don’t have an account?{' '}
+          <Link href="/sign-up">
+            <a>Sign up</a>
+          </Link>
+        </FormLink>
+      </form>
+    </FormWrapper>
+  )
+}
 
 export default FormSignIn
