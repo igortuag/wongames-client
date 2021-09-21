c89524c35b0cc8b4f0298fae3a951b4fa9085782
Tuag <igortuag@gmail.com>
2021-09-21 07:30:04 -0300

    add method to sign out

--- src/components/ProfileMenu/index.tsx
+++ src/components/ProfileMenu/index.tsx
@@ -4,6 +4,7 @@ import {
   FormatListBulleted,
   ExitToApp
 } from '@styled-icons/material-outlined'
+import { signOut } from 'next-auth/client'
 import Link from 'next/link'
 import * as S from './styles'
 
@@ -34,12 +35,10 @@ const ProfileMenu = ({ activeLink }: ProfileMenuProps) => (
       </S.Link>
     </Link>
 
-    <Link href="/logout" passHref>
-      <S.Link>
-        <ExitToApp size={24} />
-        <span>Sign out</span>
-      </S.Link>
-    </Link>
+    <S.Link role="button" onClick={() => signOut()}>
+      <ExitToApp size={24} />
+      <span>Sign out</span>
+    </S.Link>
   </S.Nav>
 )
 
