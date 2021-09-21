ceb987c7b02e0c4d262e51650d0cdfe48e0e25da
Tuag <igortuag@gmail.com>
2021-09-21 07:30:31 -0300

    add style cursor pointer

--- src/components/ProfileMenu/styles.ts
+++ src/components/ProfileMenu/styles.ts
@@ -33,6 +33,7 @@ type LinkProps = {
 
 export const Link = styled.a<LinkProps>`
   ${({ theme, isActive }) => css`
+    cursor: pointer;
     text-decoration: none;
     display: flex;
     align-items: center;
