620612c33d379fd934938213fc9558f942089df2
Tuag <igortuag@gmail.com>
2021-09-23 07:03:02 -0300

    add fieldsValidation to email and password

--- src/utils/validations/index.ts
+++ src/utils/validations/index.ts
@@ -1,6 +1,13 @@
 import { UsersPermissionsRegisterInput } from 'graphql/generated/globalTypes'
 import Joi from 'joi'
 
+const fieldsValidation = {
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required(),
+  password: Joi.string().required()
+}
+
 export function signUpValidate(values: UsersPermissionsRegisterInput) {}
 
 type SignInValues = Omit<UsersPermissionsRegisterInput, 'username'>
