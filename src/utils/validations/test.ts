9daba0aa5ea12b8c2bd610330fe2110f8886ad6a
Tuag <igortuag@gmail.com>
2021-09-23 06:54:16 -0300

    add test should validate empty fields

--- src/utils/validations/test.ts
+++ src/utils/validations/test.ts
@@ -1,1 +1,17 @@
 import { signUpValidate, signInValidate } from '.'
+
+describe('validations', () => {
+  describe('signInValidate', () => {
+    it('should validate empty fields', () => {
+      const values = {
+        email: '',
+        password: ''
+      }
+
+      expect(signInValidate(values)).toMatchObject({
+        email: 'Email is not allowed to be empty',
+        password: 'Password is not allowed to be empty'
+      })
+    })
+  })
+})
