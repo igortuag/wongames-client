2372d587af6c351279cfc4693eb30ca568271ee1
Tuag <igortuag@gmail.com>
2021-09-23 07:01:34 -0300

    add types to signUpValidate

--- src/utils/validations/index.ts
+++ src/utils/validations/index.ts
@@ -1,7 +1,7 @@
 import { UsersPermissionsRegisterInput } from 'graphql/generated/globalTypes'
 import Joi from 'joi'
 
-export function signUpValidate() {}
+export function signUpValidate(values: UsersPermissionsRegisterInput) {}
 
 type SignInValues = Omit<UsersPermissionsRegisterInput, 'username'>
 export function signInValidate(values: SignInValues) {}
