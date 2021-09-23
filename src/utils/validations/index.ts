3e45a1a0e570f7ea5503d7094db624d297e9a668
Tuag <igortuag@gmail.com>
2021-09-23 07:48:33 -0300

    add remain sign up fields on fields validation

--- src/utils/validations/index.ts
+++ src/utils/validations/index.ts
@@ -2,10 +2,12 @@ import { UsersPermissionsRegisterInput } from 'graphql/generated/globalTypes'
 import Joi from 'joi'
 
 const fieldsValidation = {
+  username: Joi.string().min(5).required(),
   email: Joi.string()
     .email({ tlds: { allow: false } })
     .required(),
-  password: Joi.string().required()
+  password: Joi.string().required(),
+  confirm_password: Joi.string().required()
 }
 
 export type FieldErros = {
