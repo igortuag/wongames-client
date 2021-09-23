312d80f2f135699e0e28ed29713b12a4a0070d9d
Tuag <igortuag@gmail.com>
2021-09-23 07:43:39 -0300

    add toMatchInlineSnapshot to validate email

--- src/utils/validations/test.ts
+++ src/utils/validations/test.ts
@@ -13,5 +13,15 @@ describe('validations', () => {
         password: 'password is not allowed to be empty'
       })
     })
+    it('should return invalid email error', () => {
+      const values = {
+        email: 'invalid-email',
+        password: '1234'
+      }
+
+      expect(signInValidate(values).email).toMatchInlineSnapshot(
+        `"\\"email\\" must be a valid email"`
+      )
+    })
   })
 })
