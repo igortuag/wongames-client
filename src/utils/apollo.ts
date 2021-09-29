2e34b72485db91da8e40aa3e7efdf72ea0818500
Tuag <igortuag@gmail.com>
2021-09-29 07:18:25 -0300

    add authorization to link apollo client

--- src/utils/apollo.ts
+++ src/utils/apollo.ts
@@ -2,25 +2,30 @@ import { ApolloClient, HttpLink, NormalizedCacheObject } from '@apollo/client'
 import { setContext } from '@apollo/client/link/context'
 import { useMemo } from 'react'
 import apolloCache from './apolloCache'
-import { session } from 'next-auth/client'
+import { Session } from 'next-auth/client'
 
 let apolloClient: ApolloClient<NormalizedCacheObject | null>
 
-function createApolloClient(session?: session | null) {
+function createApolloClient(session?: Session | null) {
   const httpLink = new HttpLink({
     uri: `${process.env.NEXT_PUBLIC_API_URL}/graphql`
   })
 
+  const authLink = setContext((_, { headers }) => {
+    const authorization = session?.jwt ? `Bearer ${session.jwt}` : null
+    return { headers: { authorization, ...headers } }
+  })
+
   return new ApolloClient({
     ssrMode: typeof window === 'undefined',
-    link: httpLink,
+    link: authLink.concat(httpLink),
     cache: apolloCache
   })
 }
 
 export function initializeApollo(
   initialState = null,
-  session?: session | null
+  session?: Session | null
 ) {
   // serve para verificar se já existe uma instância, para não criar outra
   const apolloClientGlobal = apolloClient ?? createApolloClient(session)
