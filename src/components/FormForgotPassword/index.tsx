7cb246fc278e0ca2d07e551a113c8b80c258171d
Tuag <igortuag@gmail.com>
2021-10-01 06:49:10 -0300

    add FormForgotPassword as copy form sign in

---
+++ src/components/FormForgotPassword/index.tsx
@@ -0,0 +1,103 @@
+import { useState } from 'react'
+import { signIn } from 'next-auth/client'
+import { useRouter } from 'next/router'
+
+import Link from 'next/link'
+import { Email, Lock, ErrorOutline } from '@styled-icons/material-outlined'
+import Button from 'components/Button'
+
+import { FormWrapper, FormLink, FormLoading, FormError } from 'components/Form'
+import TextField from 'components/TextField'
+import * as S from './styles'
+import { FieldErros, signInValidate } from 'utils/validations'
+
+const FormSignIn = () => {
+  const [formError, setFormError] = useState('')
+  const [fieldError, setFieldError] = useState<FieldErros>({
+    email: '',
+    password: ''
+  })
+  const [values, setValues] = useState({
+    email: '',
+    password: ''
+  })
+  const [loading, setLoading] = useState(false)
+  const routes = useRouter()
+  const { push, query } = routes
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault()
+    setLoading(true)
+
+    const errors = signInValidate(values)
+
+    if (Object.keys(errors).length > 0) {
+      setFieldError(errors)
+      setLoading(false)
+      return
+    }
+    setFieldError({ email: '', password: '' })
+    const result = await signIn('credentials', {
+      ...values,
+      redirect: false,
+      callbackUrl: `${window.location.origin}${query?.callbackUrl || ''}`
+    })
+
+    if (result?.url) {
+      return push(result?.url)
+    }
+    setLoading(false)
+
+    setFormError('Username or password is invalid')
+  }
+
+  const handleInput = (field: string, value: string) => {
+    setValues((s) => ({ ...s, [field]: value }))
+  }
+
+  return (
+    <FormWrapper>
+      {!!formError && (
+        <FormError>
+          {' '}
+          <ErrorOutline />
+          {formError}
+        </FormError>
+      )}
+      <form onSubmit={handleSubmit}>
+        <TextField
+          name="email"
+          placeholder="Email"
+          type="email"
+          error={fieldError.email}
+          onInputChange={(v) => handleInput('email', v)}
+          icon={<Email />}
+        />
+        <TextField
+          name="password"
+          placeholder="Password"
+          type="password"
+          error={fieldError.password}
+          onInputChange={(v) => handleInput('password', v)}
+          icon={<Lock />}
+        />
+        <Link href="/forgot-password" passHref>
+          <S.ForgotPassword>Forgot your password?</S.ForgotPassword>
+        </Link>
+
+        <Button type="submit" size="large" fullWidth disabled={loading}>
+          {loading ? <FormLoading /> : <span>Sign in now</span>}
+        </Button>
+
+        <FormLink>
+          Don’t have an account?
+          <Link href="/sign-up">
+            <s>Sign up</s>
+          </Link>
+        </FormLink>
+      </form>
+    </FormWrapper>
+  )
+}
+
+export default FormSignIn
--- src/pages/forgot-password.jsx
+++ src/pages/forgot-password.jsx
@@ -1,10 +1,10 @@
-import FormSignIn from 'components/FormSignIn'
+import FormForgotPassword from 'components/FormForgotPassword'
 import Auth from 'templates/Auth'
 
 export default function ForgotPassword() {
   return (
     <Auth title="Request new password">
-      <FormSignIn />
+      <FormForgotPassword />
     </Auth>
   )
 }
