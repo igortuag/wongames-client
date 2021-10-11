f9d9793f7a0e0134af3dccda2a026edeb37616a5
Tuag <igortuag@gmail.com>
2021-10-11 06:50:18 -0300

    LoginReqBody to handlers forgot password

--- src/utils/mockServer/handlers.ts
+++ src/utils/mockServer/handlers.ts
@@ -1,3 +1,14 @@
 import { rest } from 'msw'
 
-export const handlers = []
+type LoginReqBody = {
+  email: string
+}
+
+export const handlers = [
+  rest.post<LoginReqBody>(
+    `${process.env.NEXT_PUBLIC_API_URL}/auth/forgot-password`,
+    (req, res, ctx) => {
+      const { email } = req.body
+    }
+  )
+]
