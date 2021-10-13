72ace8e7a79eef03f91189200d88305eb9b87d48
Tuag <igortuag@gmail.com>
2021-10-13 07:08:09 -0300

    add ResetReqBody type

--- src/utils/mockServer/handlers.ts
+++ src/utils/mockServer/handlers.ts
@@ -4,9 +4,47 @@ type LoginReqBody = {
   email: string
 }
 
+type ResetReqBody = {
+  code: string
+  password: string
+  passwordConfirmation: string
+}
+
 export const handlers = [
   rest.post<LoginReqBody>(
     `${process.env.NEXT_PUBLIC_API_URL}/auth/forgot-password`,
+    (req, res, ctx) => {
+      const { email } = req.body
+
+      if (email === 'false@email.com') {
+        return res(
+          ctx.status(400),
+          ctx.json({
+            error: 'Bad Request',
+            message: [
+              {
+                messages: [
+                  {
+                    message: 'This email does not exist'
+                  }
+                ]
+              }
+            ]
+          })
+        )
+      }
+
+      return res(
+        ctx.status(200),
+        ctx.json({
+          ok: true
+        })
+      )
+    }
+  )
+
+  rest.post<ResetReqBody>(
+    `${process.env.NEXT_PUBLIC_API_URL}/auth/reset-password`,
     (req, res, ctx) => {
       const { email } = req.body
 
