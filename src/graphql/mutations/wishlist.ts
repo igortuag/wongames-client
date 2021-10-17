c9ad3f6591cc94d27cbf8de5dd6c388d61c4d10e
Tuag <igortuag@gmail.com>
2021-10-17 07:01:21 -0300

    add mutation to update wishlist

--- src/graphql/mutations/wishlist.ts
+++ src/graphql/mutations/wishlist.ts
@@ -1,8 +1,23 @@
 import { gql } from '@apollo/client'
 
-export const MUTATION_WISHILIST = gql`
+export const MUTATION_CREATE_WISHILIST = gql`
   mutation MutationCreateWishlist($input: createWishList!) {
     createWishList(input: $input) {
+      id
+      user {
+        id
+        username
+      }
+      games {
+        id
+        name
+      }
+  }
+`
+
+export const MUTATION_UPDATE_WISHILIST = gql`
+  mutation MutationCreateWishlist($input: updateWishList!) {
+    updateWishList(input: $input) {
       id
       user {
         id
