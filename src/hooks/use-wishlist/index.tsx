066a0199bcf1c36032aeb81058e66ecfce5e0350
Tuag <igortuag@gmail.com>
2021-10-23 08:32:45 -0300

    add initial structure to use wishlist

---
+++ src/hooks/use-wishlist/index.tsx
@@ -0,0 +1,22 @@
+import { GameCardProps } from 'components/GameCard'
+import { createContext } from 'react'
+
+export type WishlistContextData = {
+  items: GameCardProps[]
+  isInWishlist: (id: string) => boolean
+  addToWishlist: (id: string) => void
+  removeFromWishlist: (id: string) => void
+  loadign: boolean
+}
+
+export const WishlistContextDefaultValues = {
+  item: [],
+  isInWishlist: () => false,
+  addToWishlist: () => null,
+  removeFromWishlist: () => null,
+  loading: false
+}
+
+export const WishlistContext = createContext<WishlistContextData>(
+  WishlistContextDefaultValues
+)
