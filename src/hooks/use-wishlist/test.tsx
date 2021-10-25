38cb26b1a11c236df3efac9b9dc5ee23a5f21243
Tuag <igortuag@gmail.com>
2021-10-25 07:42:05 -0300

    add mocked provider to test wishlsit

---
+++ src/hooks/use-wishlist/test.tsx
@@ -0,0 +1,13 @@
+import { MockedProvider } from '@apollo/client/testing'
+import React from 'react'
+import { useWishlist, WishlistProvider } from '.'
+
+describe('useWishlist', () => {
+  it('should return wishlist items', () => {
+    const wrapper = ({ children }: { children: React.ReactNode }) => (
+      <MockedProvider mocks={[]}>
+        <WishlistProvider>{children}</WishlistProvider>
+      </MockedProvider>
+    )
+  })
+})
