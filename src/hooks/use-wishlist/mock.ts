a96ab73ce3d08b1c2071940348ac3b5f7ffde32c
Tuag <igortuag@gmail.com>
2021-10-25 07:46:57 -0300

    add function gameMock

--- src/hooks/use-wishlist/mock.ts
+++ src/hooks/use-wishlist/mock.ts
@@ -1,5 +1,17 @@
 import { QUERY_WISHLIST } from 'graphql/queries/wishlist'
 
+const gameMock = (id: string) => ({
+  id,
+  name: 'Sample Game',
+  slug: 'sample-game',
+  price: 10.5,
+  developers: [{ name: 'sample developer ' }],
+  cover: {
+    src: '/sample-game-jpg'
+  },
+  __typename: 'Game'
+})
+
 export const wishlistMock = {
   request: {
     query: QUERY_WISHLIST,
