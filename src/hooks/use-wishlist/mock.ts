eb132b9ad7e05a9b03ebb9372bc105c78427ffd7
Tuag <igortuag@gmail.com>
2021-10-25 07:49:35 -0300

    export wishlistItems

--- src/hooks/use-wishlist/mock.ts
+++ src/hooks/use-wishlist/mock.ts
@@ -32,3 +32,30 @@ export const wishlistMock = {
     }
   }
 }
+
+export const wishlistItems = [
+  {
+    id: '1',
+    title: 'Sample Game 1',
+    slug: 'sample-game-1',
+    developer: 'sample developer',
+    img: 'http://localhost:1337/sample-game.jpg',
+    price: 10.5
+  },
+  {
+    id: '2',
+    title: 'Sample Game 2',
+    slug: 'sample-game-2',
+    developer: 'sample developer',
+    img: 'http://localhost:1337/sample-game.jpg',
+    price: 10.5
+  },
+  {
+    id: '3',
+    title: 'Sample Game 3',
+    slug: 'sample-game-3',
+    developer: 'sample developer',
+    img: 'http://localhost:1337/sample-game.jpg',
+    price: 10.5
+  }
+]
