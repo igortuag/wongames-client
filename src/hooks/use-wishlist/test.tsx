101f150710f6aebc1c6dcb05c3a6848934f4d31a
Tuag <igortuag@gmail.com>
2021-10-26 08:02:48 -0300

    add useSession spy on next auth client useSession

--- src/hooks/use-wishlist/test.tsx
+++ src/hooks/use-wishlist/test.tsx
@@ -3,6 +3,9 @@ import { renderHook } from '@testing-library/react-hooks'
 import React from 'react'
 import { useWishlist, WishlistProvider } from '.'
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const useSession = jest.spyOn(require('next-auth/client'), 'useSession')
+
 describe('useWishlist', () => {
   it('should return wishlist items', () => {
     const wrapper = ({ children }: { children: React.ReactNode }) => (
