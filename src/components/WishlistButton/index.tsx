dede94e13ff0856394485e3f9a6e988cfe5bd00c
Tuag <igortuag@gmail.com>
2021-10-31 07:45:26 -0300

    add initial strucuture to component

---
+++ src/components/WishlistButton/index.tsx
@@ -0,0 +1,11 @@
+import Button from 'components/Button'
+
+type WishlistButtonProps = {
+  id: string
+}
+
+const WishlistButton = ({ id }: WishlistButtonProps) => {
+  return <Button></Button>
+}
+
+export default WishlistButton
