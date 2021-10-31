7e11d60fe3b06f92518df1ecbb87b463df303917
Tuag <igortuag@gmail.com>
2021-10-31 08:15:10 -0300

    adjust jest use session mock to tests

--- src/components/WishlistButton/test.tsx
+++ src/components/WishlistButton/test.tsx
@@ -2,6 +2,11 @@ import { WishlistContextDefaultValues } from 'hooks/use-wishlist'
 import { render, screen } from 'utils/test-utils'
 import WishlistButton from '.'
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const useSession = jest.spyOn(require('next-auth/client'), 'useSession')
+const session = { jwt: '123', user: { email: 'lorem@ipsum.com' } }
+useSession.mockReturnValue(() => [session])
+
 describe('WishlistButton', () => {
   it('should renders a button to add to wishlist', () => {
     const wishlistProviderProps = {
@@ -50,8 +55,7 @@ describe('WishlistButton', () => {
   it('should not render if not logged', () => {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const useSession = jest.spyOn(require('next-auth/client'), 'useSession')
-    useSession.mockReturnValue(() => [null])
-
+    useSession.mockReturnValueOnce(() => [null])
     const wishlistProviderProps = {
       ...WishlistContextDefaultValues,
       isInWishlist: () => false
