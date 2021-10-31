110991b30a888d7eb7a8582791d6bdd896d32e38
Tuag <igortuag@gmail.com>
2021-10-31 07:43:30 -0300

    should renders a button to add to wishlist test

--- src/components/WishlistButton/test.tsx
+++ src/components/WishlistButton/test.tsx
@@ -1,5 +1,16 @@
+import { WishlistContextDefaultValues } from 'hooks/use-wishlist'
+import { render, screen } from 'utils/test-utils'
 import WishlistButton from '.'
 
 describe('WishlistButton', () => {
-  it('should renders a button to add to wishlist', () => {})
+  it('should renders a button to add to wishlist', () => {
+    const wishlistProviderProps = {
+      ...WishlistContextDefaultValues,
+      isInWishlist: () => false
+    }
+
+    render(<WishlistButton id="1" />, { wishlistProviderProps })
+
+    expect(screen.getByLabelText(/add to wishlist/)).toBeInTheDocument()
+  })
 })
