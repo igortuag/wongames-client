c49bf01f2c73d18e4a6103212b20eb91cbcb0488
Tuag <igortuag@gmail.com>
2021-10-31 07:48:07 -0300

    add icon is in wishlist

--- src/components/WishlistButton/index.tsx
+++ src/components/WishlistButton/index.tsx
@@ -1,11 +1,26 @@
+import { Favorite, FavoriteBorder } from '@styled-icons/material-outlined'
+
 import Button from 'components/Button'
+import { useWishlist } from 'hooks/use-wishlist'
 
 type WishlistButtonProps = {
   id: string
 }
 
 const WishlistButton = ({ id }: WishlistButtonProps) => {
-  return <Button></Button>
+  const { isInWishlist } = useWishlist()
+
+  return (
+    <Button
+      icon={
+        isInWishlist(id) ? (
+          <Favorite aria-label="Remove from wishlist" />
+        ) : (
+          <FavoriteBorder aria-label="Add to wishlist" />
+        )
+      }
+    />
+  )
 }
 
 export default WishlistButton
