363774196fab9f9a838038d734dc36a2f46ae384
Tuag <igortuag@gmail.com>
2021-10-31 07:59:57 -0300

    add condition to not return button if there is no session

--- src/components/WishlistButton/index.tsx
+++ src/components/WishlistButton/index.tsx
@@ -2,6 +2,7 @@ import { Favorite, FavoriteBorder } from '@styled-icons/material-outlined'
 
 import Button, { ButtonProps } from 'components/Button'
 import { useWishlist } from 'hooks/use-wishlist'
+import { useSession } from 'next-auth/client'
 
 type WishlistButtonProps = {
   id: string
@@ -13,11 +14,14 @@ const WishlistButton = ({
   hasText,
   size = 'small'
 }: WishlistButtonProps) => {
+  const [session] = useSession()
   const { isInWishlist } = useWishlist()
 
   const buttonText = isInWishlist(id)
     ? 'Remove from wishlist'
     : 'Add to wishlist'
+
+  if (!session) return null
 
   return (
     <Button
