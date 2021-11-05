671d36bbf0c3b5873bfd4781a162589b8ac7400a
Tuag <igortuag@gmail.com>
2021-11-05 07:32:07 -0300

    fix should render buttons test

--- src/components/GameInfo/test.tsx
+++ src/components/GameInfo/test.tsx
@@ -30,7 +30,7 @@ describe('<GameInfo />', () => {
       screen.getByRole('button', { name: /add to cart/i })
     ).toBeInTheDocument()
     expect(
-      screen.getByRole('button', { name: /wishlist/i })
+      screen.getByRole('button', { name: /add to wishlist/i })
     ).toBeInTheDocument()
   })
 })
