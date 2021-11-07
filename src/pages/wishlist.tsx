5b08cf1163557e465e4f8e9a2e0c25100b26c2fa
Tuag <igortuag@gmail.com>
2021-11-07 09:14:56 -0300

    add query and pass initial apollo state

--- src/pages/wishlist.tsx
+++ src/pages/wishlist.tsx
@@ -8,6 +8,11 @@ import { QUERY_RECOMMENDED } from 'graphql/queries/recommended'
 import { gamesMapper } from 'utils/mappers'
 import { GetServerSidePropsContext } from 'next'
 import protectedRoutes from 'utils/protected-routes'
+import {
+  QueryWishlist,
+  QueryWishlistVariables
+} from 'graphql/generated/QueryWishlist'
+import { QUERY_WISHLIST } from 'graphql/queries/wishlist'
 
 export default function WishlistPage(props: WishlistTemplateProps) {
   return <Wishlist {...props} />
@@ -19,17 +24,24 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 
   if (!session) return {}
 
+  await apolloCliente.query<QueryWishlist, QueryWishlistVariables>({
+    query: QUERY_WISHLIST,
+    variables: {
+      identifier: session.user.email as string
+    }
+  })
+
   const { data } = await apolloCliente.query<QueryRecommended>({
     query: QUERY_RECOMMENDED
   })
 
   return {
     props: {
-      games: gamesMock,
+      session,
+      initializeApollo: apolloCliente.cache.extract(),
       recommendedTitle: data.recommended?.section?.highlight?.title,
       recommendedGames: gamesMapper(data.recommended?.section?.games),
-      recommendedHighlights: highlightMock,
-      session
+      recommendedHighlights: highlightMock
     }
   }
 }
