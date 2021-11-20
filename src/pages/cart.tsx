d5c19d15011bdcf5185eecdbefb263d6b0303504
Tuag <igortuag@gmail.com>
2021-11-20 09:10:15 -0300

    remvoe non used props

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,6 +1,5 @@
 import Cart, { CartProps } from 'templates/Cart'
 
-import itemsMock from 'components/CartList/mock'
 import highlightMock from 'components/Highlight/mock'
 import cardsMock from 'components/PaymentOptions/mock'
 import { initializeApollo } from 'utils/apollo'
@@ -25,8 +24,6 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
   return {
     props: {
       session,
-      items: itemsMock,
-      total: '$ 430,00',
       cards: cardsMock,
       recommendedTitle: data.recommended?.section?.highlight?.title,
       recommendedGames: gamesMapper(data.recommended?.section?.games),
