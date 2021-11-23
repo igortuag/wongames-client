6552d6967b2e87781492ff964532db5a39c31aed
Tuag <igortuag@gmail.com>
2021-11-23 07:49:48 -0300

    add styles to error

--- src/components/PaymentForm/styles.ts
+++ src/components/PaymentForm/styles.ts
@@ -28,6 +28,12 @@ export const Footer = styled.div`
   `}
 `
 
-export const Error = styled.div`
-  ${({ theme }) => css``}
+export const Error = styled.p`
+  ${({ theme }) => css`
+    color: ${theme.colors.red};
+    font-size: ${theme.font.sizes.xsmall};
+    padding-top: ${theme.spacings.xsmall};
+    display: flex;
+    align-items: center;
+  `}
 `
