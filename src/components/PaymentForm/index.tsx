dab3daf8484b5caf2efb9fa8b793c1b43f6c2d94
Tuag <igortuag@gmail.com>
2021-11-23 07:47:00 -0300

    add method handle change and error state

--- src/components/PaymentForm/index.tsx
+++ src/components/PaymentForm/index.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react'
+import { StripeCardElementChangeEvent } from '@stripe/stripe-js'
+
 import { CardElement } from '@stripe/react-stripe-js'
 import { ShoppingCart } from '@styled-icons/material-outlined'
 import Button from 'components/Button'
@@ -5,6 +8,10 @@ import Heading from 'components/Heading'
 import * as S from './styles'
 
 const PaymentForm = () => {
+  const [error, setError] = useState<string | null>()
+
+  const handleChange = async (event: StripeCardElementChangeEvent) => {}
+
   return (
     <S.Wrapper>
       <S.Body>
@@ -12,7 +19,10 @@ const PaymentForm = () => {
           Payment
         </Heading>
 
-        <CardElement options={{ hidePostalCode: true }} />
+        <CardElement
+          options={{ hidePostalCode: true }}
+          onChange={handleChange}
+        />
       </S.Body>
       <S.Footer>
         <Button as="a" fullWidth minimal>
