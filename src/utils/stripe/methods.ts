b8f386282c6a29257621db9104c8e8e035012b20
Tuag <igortuag@gmail.com>
2021-12-08 07:09:03 -0300

    use fetcher on createPaymentIntent

--- src/utils/stripe/methods.ts
+++ src/utils/stripe/methods.ts
@@ -27,17 +27,9 @@ export const createPaymentIntent = async ({
   items,
   token
 }: PaymentIntentParams) => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/orders/create-payment-intent`,
-    {
-      method: 'POST',
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ cart: items })
-    }
-  )
-
-  return await response.json()
+  return fetcher({
+    url: `/orders/create-payment-intent`,
+    body: JSON.stringify({ cart: items }),
+    token
+  })
 }
