fa0f0875aec2f04cbb757e95d1c7a8eaef9a54ba
Igor Tuag <igortuag@gmail.com>
2021-12-13 10:39:58 -0300

    map array of orders to return right structure

--- src/utils/mappers/index.ts
+++ src/utils/mappers/index.ts
@@ -67,5 +67,31 @@ export const cartMapper = (games: QueryGames_games[] | undefined) => {
 }
 
 export const ordersMapper = (orders?: QueryOrders_orders[]) => {
-  return orders ? orders : []
+  return orders
+    ? orders.map((order) => {
+        return {
+          id: order.id,
+          paymentInfo: {
+            flag: order.card_brand,
+            img: order.card_brand ? `/img/cards/${order.card_brand}.png` : null,
+            number: order.card_last4
+              ? `**** **** **** ${order.card_last4}`
+              : 'Free Game',
+            purchaseDate: `Purchase made on ${new Intl.DateTimeFormat('en-US', {
+              day: 'numeric',
+              month: 'short',
+              year: 'numeric'
+            }).format(new Date(order.created_at))}`
+          },
+          games: order.games.map((game) => ({
+            id: game.id,
+            title: game.name,
+            downloadLink:
+              'https://wongames.com/game/download/yuYT56Tgh431LkjhNBgdf',
+            img: `http://localhost:1337${game.cover?.url}`,
+            price: formatPrice(game.price)
+          }))
+        }
+      })
+    : []
 }
