22f3d62c4759b9799d5578614c2bbebdf0d7e156
Igor Tuag <igortuag@gmail.com>
2021-12-14 07:17:19 -0300

    fetching data orders

--- src/pages/profile/orders.tsx
+++ src/pages/profile/orders.tsx
@@ -5,6 +5,11 @@ import ordersMock from 'components/OrdersList/mock'
 import protectedRoutes from 'utils/protected-routes'
 import { GetServerSidePropsContext } from 'next'
 import { initializeApollo } from 'utils/apollo'
+import {
+  QueryOrders,
+  QueryOrdersVariables
+} from 'graphql/generated/QueryOrders'
+import { QUERY_ORDERS } from 'graphql/queries/orders'
 
 export default function Orders({ items }: OrdersListProps) {
   return (
@@ -17,6 +22,13 @@ export default function Orders({ items }: OrdersListProps) {
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const session = await protectedRoutes(context)
   const apolloClient = initializeApollo(null, session)
+
+  const { data } = await apolloClient.query<QueryOrders, QueryOrdersVariables>({
+    query: QUERY_ORDERS,
+    variables: {
+      identifier: session?.id
+    }
+  })
 
   return {
     props: {
