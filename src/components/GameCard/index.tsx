fc6fe28d83b83a1420439bc87f87ff63a662720c
Tuag <igortuag@gmail.com>
2021-12-23 07:02:41 -0300

    add image from next image

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 import Ribbon, { RibbonColors, RibbonSizes } from 'components/Ribbon'
 import * as S from './styles'
@@ -41,7 +42,7 @@ const GameCard = ({
     )}
     <Link href={`/game/${slug}`} passHref>
       <S.ImageBox>
-        <img src={img} alt={title} />
+        <Image src={img} alt={title} layout="fill" />
       </S.ImageBox>
     </Link>
     <S.Content>
