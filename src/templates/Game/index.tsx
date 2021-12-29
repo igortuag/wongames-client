06ebe9003d5b70d68e08d8be7eac0e7907650cec
Tuag <igortuag@gmail.com>
2021-12-29 06:55:38 -0300

    add NextSEO dynamic to game page

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,4 +1,6 @@
 import Gallery, { GalleryImageProps } from 'components/Gallery'
+import { NextSeo } from 'next-seo'
+
 import GameInfo, { GameInfoProps } from 'components/GameInfo'
 import GameDetails, { GameDetailsProps } from 'components/GameDetails'
 import { GameCardProps } from 'components/GameCard'
@@ -38,6 +40,22 @@ const Game = ({
   recommendGames
 }: GameTemplateProps) => (
   <Base>
+    <NextSeo
+      title={`${gameInfo.title} - Won Games`}
+      description={gameInfo.description}
+      canonical={`https://won-games.com/game/${slug}`}
+      openGraph={{
+        url: `https://won-games.com/game/${slug}`,
+        title: `${gameInfo.title} - Won Games`,
+        description: gameInfo.description,
+        images: [
+          {
+            url: cover,
+            alt: gameInfo.title
+          }
+        ]
+      }}
+    />
     <S.Cover src={cover} role="image" aria-label="cover"></S.Cover>
     <S.Main>
       <S.SectionGameInfo>
