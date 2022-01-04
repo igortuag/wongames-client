9187d6f8f6c8e63f22f7a8ec59e0833fa5fb95a2
Tuag <igortuag@gmail.com>
2022-01-04 06:57:17 -0300

    add reference to cy type

--- cypress/integration/example.ts
+++ cypress/integration/example.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+/// <reference path="../support/index.d.ts" />
+
 describe('Cypress TS', () => {
   it('should go to Google', () => {
     cy.google()
