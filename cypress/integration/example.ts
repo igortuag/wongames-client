78e27ab08477e8f51ba981c7d64d2bdaed568a7e
Tuag <igortuag@gmail.com>
2022-01-06 07:14:58 -0300

    add test on wongames prod

--- cypress/integration/example.ts
+++ cypress/integration/example.ts
@@ -6,12 +6,18 @@ describe('Cypress TS', () => {
     cy.google()
   })
 
-  it('should change light/dark theme on willian justen site', () => { 
+  it.skip('should change light/dark theme on willian justen site', () => { 
     cy.visit('https://willianjusten.com.br/')
 
     cy.findByTitle(/Mudar o tema/i).click()
     cy.get('.light').should('exist')
     cy.findByTitle(/Mudar o tema/i).click()
     cy.get('.dark').should('exist')
+  })
+
+  it('should vist won games', () => {
+    cy.visit('https://wongames.willianjusten.com.br/')
+
+    cy.findByText(/Esse é um site de estudos!/i).should('exist')
   })
 })
