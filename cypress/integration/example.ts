9ca5e46d062be1276a85a77fd215a4f974ca3560
Tuag <igortuag@gmail.com>
2022-01-06 07:13:07 -0300

    add example using testing library

--- cypress/integration/example.ts
+++ cypress/integration/example.ts
@@ -2,7 +2,16 @@
 /// <reference path="../support/index.d.ts" />
 
 describe('Cypress TS', () => {
-  it('should go to Google', () => {
+  it.skip('should go to Google', () => {
     cy.google()
+  })
+
+  it('should change light/dark theme on willian justen site', () => { 
+    cy.visit('https://willianjusten.com.br/')
+
+    cy.findByTitle(/Mudar o tema/i).click()
+    cy.get('.light').should('exist')
+    cy.findByTitle(/Mudar o tema/i).click()
+    cy.get('.dark').should('exist')
   })
 })
