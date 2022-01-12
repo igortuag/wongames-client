268bf46a403654d4d287630d42d8f6c3809bfc21
Tuag <igortuag@gmail.com>
2022-01-12 07:09:26 -0300

    click on the midle dot

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -6,7 +6,9 @@ describe('Home Page', () => {
 
     cy.get('.slick-slider').within(() => {
       cy.findByRole('heading', {name: /cyberpunk 2077/i})
-      cy.findByRole('link', {name: /by now/i})
+      cy.findByRole('link', { name: /by now/i })
+      
+      cy.get('.slick-dots > :nth-child(2) > button').click()
     })
   })
 })
