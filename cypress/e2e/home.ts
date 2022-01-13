b7f6c0f1788ad2aaa79c081f10f4a11a88c9fc25
Tuag <igortuag@gmail.com>
2022-01-13 06:46:00 -0300

    add command shouldRenderBanner

--- cypress/e2e/home.ts
+++ cypress/e2e/home.ts
@@ -4,21 +4,6 @@ describe('Home Page', () => {
   it('should render home sections', () => {
     cy.visit('/')
 
-    cy.get('.slick-slider').within(() => {
-      cy.findByRole('heading', {name: /cyberpunk 2077/i})
-      cy.findByRole('link', { name: /by now/i })
-      
-      cy.get('.slick-dots > :nth-child(2) > button').click()
-      cy.wait(500)
-
-      cy.findByRole('heading', { name: /horizon zero dawn/i })
-      cy.findByRole('link', { name: /by now/i })
-      
-      cy.get('.slick-dots > :nth-child(3) > button').click()
-      cy.wait(500)
-
-      cy.findByRole('heading', { name: /huge promotion/i })
-      cy.findByRole('link', { name: /browse games/i })
-    })
+    cy.shouldRenderBanner()
   })
 })
