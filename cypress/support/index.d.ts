2dcd3de4691ae484942bdcab05ab862d3252b7fb
Tuag <igortuag@gmail.com>
2022-01-15 07:30:57 -0300

    add shouldRenderShowcase type

--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,6 +1,11 @@
 // load type definitions from Cypress module
 /// <reference types="cypress" />
 
+type ShowcaseAttributes = {
+  name: string;
+  highlight?: boolean;
+}
+
 declare namespace Cypress {
   interface Chainable<Subject> {
     /**
@@ -13,5 +18,11 @@ declare namespace Cypress {
      * @example cy.shouldRenderBanner()
      */
     shouldRenderBanner(): Chainable<Element>
+    
+    /**
+     * Custom command to check banner in page
+     * @example cy.shouldRenderShowcase()
+     */
+    shouldRenderShowcase(attrs: ShowcaseAttributes): Chainable<Element>
   }
 }
