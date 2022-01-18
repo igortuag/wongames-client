26a51c7370ed50f1d9e108b591ae418d98e06847
Tuag <igortuag@gmail.com>
2022-01-18 06:49:40 -0300

    use command getByDataCy

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -55,17 +55,17 @@ Cypress.Commands.add('shouldRenderBanner', () => {
 Cypress.Commands.add('shouldRenderShowcase', ({
   name, highlight = false,
 }) => {
-  cy.get(`[data-cy="${name}"]`).within(() => {
+  cy.getByDataCy(`"${name}"`).within(() => {
     cy.findByRole('heading', { name }).should('exist')
 
-    cy.get(`[data-cy="highlight"]`).should(highlight ? 'exist' : 'not.exist')
+    cy.getByDataCy("highlight").should(highlight ? 'exist' : 'not.exist')
 
     if (highlight) {
-      cy.get(`[data-cy="highlight"]`).within(() => {
+      cy.getByDataCy("highlight").within(() => {
         cy.findByRole('link').should('have.attr', 'href')
       })
     }
 
-    cy.get(`[data-cy="game-card"]`).should('have.length.gt', 0)
+    cy.getByDataCy("game-card").should('have.length.gt', 0)
   })
 })
