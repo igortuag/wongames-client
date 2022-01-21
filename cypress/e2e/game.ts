cff71e3311e01f5515cf5664be6acab314c6f3e7
Tuag <igortuag@gmail.com>
2022-01-21 07:15:22 -0300

    check the text

--- cypress/e2e/game.ts
+++ cypress/e2e/game.ts
@@ -5,7 +5,8 @@ describe('Home Page', () => {
     cy.visit('/game//cyberpunk-2077')
 
     cy.getByDataCy('game-info').within(() => {
-      cy,findByRole('heading', { name: 'Cyberpunk 2077' }).should('exist')
+      cy.findByRole('heading', { name: 'Cyberpunk 2077' }).should('exist')
+      cy.findByText(/Cyberpunk 2077 is an open world/i).should('exist')
     })
   })
 })
