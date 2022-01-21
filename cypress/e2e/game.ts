33d9c17945115ace16314b118e573a9ae562141e
Tuag <igortuag@gmail.com>
2022-01-21 07:13:56 -0300

    verify if exist heading

--- cypress/e2e/game.ts
+++ cypress/e2e/game.ts
@@ -3,5 +3,9 @@
 describe('Home Page', () => {
   it('should render home sections', () => {
     cy.visit('/game//cyberpunk-2077')
+
+    cy.getByDataCy('game-info').within(() => {
+      cy,findByRole('heading', { name: 'Cyberpunk 2077' }).should('exist')
+    })
   })
 })
