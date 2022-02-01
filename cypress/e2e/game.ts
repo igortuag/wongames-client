6498bd8370255be108ff8fe10ab7b85ed5780d0b
Tuag <igortuag@gmail.com>
2022-02-01 07:06:51 -0300

    add tests e2e to game details

--- cypress/e2e/game.ts
+++ cypress/e2e/game.ts
@@ -18,7 +18,26 @@ describe('Home Page', () => {
     cy.getByDataCy('content').within(() => {
       cy.findByRole('heading', { name: /description/i }).should('exist')
     })
-
     cy.getByDataCy('content').children().should('have.length.at.least', 2)
+
+    // Details
+    cy.getByDataCy('game-details').within(() => {
+      cy.findByRole('heading', { name: /game details/i }).should('exist')
+      cy.findByRole('heading', { name: /developer/i }).should('exist')
+      cy.findByRole('heading', { name: /release date/i }).should('exist')
+      cy.findByRole('heading', { name: /platforms/i }).should('exist')
+      cy.findByRole('heading', { name: /publisher/i }).should('exist')
+      cy.findByRole('heading', { name: /rating/i }).should('exist')
+      cy.findByRole('heading', { name: /genres/i }).should('exist')
+
+      cy.findAllByText(/cd projekt red/i).should('have.length', 2)
+      cy.findByText(/dec 8, 2020/i).should('exist')
+      cy.findByRole('img', { name: /windows/i }).should('exist')
+      cy.findByText(/free/i).should('exist')
+      cy.findByText('Role-playing / Action / Sci-fi').should('exist')
+    })
+
+    cy.shouldRenderShowcase({ name: "Upcoming Games",highlight: true })
+    cy.shouldRenderShowcase({ name: "You may like these games", highlight: false })
   })
 })
