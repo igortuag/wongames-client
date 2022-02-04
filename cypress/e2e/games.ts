4950145f41978931558fe8e72dab96ef55d03618
Tuag <igortuag@gmail.com>
2022-02-04 07:16:20 -0300

    verify if filter sort by price exist

--- cypress/e2e/games.ts
+++ cypress/e2e/games.ts
@@ -4,4 +4,8 @@ describe('Explore page', () => {
   before(() => {
     cy.visit('/games')
   })
+
+  it('should render filters columns', () => {
+    cy.findByRole('heading', { name: /sort by price/i }).should('exist')
+  })
 })
