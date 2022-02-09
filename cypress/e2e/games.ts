3da8b93e57bdeeba173e2720f2cecd37dc61f571
Tuag <igortuag@gmail.com>
2022-02-09 06:43:43 -0300

    add regex to find price

--- cypress/e2e/games.ts
+++ cypress/e2e/games.ts
@@ -39,7 +39,7 @@ describe('Explore page', () => {
     cy.location('href').should('contain', 'sort-price%3Adesc')
 
     cy.getByDataCy('game-card').first().within(() => {
-      cy.findByText("$0.00").should('not.exist')
+      cy.findByText(/^\$\d+(\.\d{1,2})?/)
     })
   })
 })
