917be1399321512e280a44b08899216b5b701971
Tuag <igortuag@gmail.com>
2022-02-14 07:31:54 -0300

    add method to create fake user

--- cypress/support/generate.ts
+++ cypress/support/generate.ts
@@ -5,3 +5,15 @@ export type User = {
   email: string
   password: string
 }
+
+export const createUser = build<User>('User', {
+  fields: {
+    username: fake(f => f.internet.userName),
+    password: fake(f => f.internet.password),
+    email: ''
+  },
+  postBuild: user => ({
+    ...user,
+    email: `${user.username.toLowerCase()}+e2e@wongames.com`
+  })
+})
