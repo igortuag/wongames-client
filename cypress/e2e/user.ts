7358cb3006006ff05548d987d230859ce7e5b78f
Tuag <igortuag@gmail.com>
2022-02-14 07:33:09 -0300

    create user on test e2e

--- cypress/e2e/user.ts
+++ cypress/e2e/user.ts
@@ -1,11 +1,13 @@
 /// <reference path="../support/index.d.ts" />
 
-describe('User', () => {
-  before(() => {
-    cy.visit('/sign-up')
-  })
+import { createUser } from '../support/generate'
 
+describe('User', () => {
   it('should sing up', () => {
+    const user = createUser()
+
+    cy.visit('/sign-up')
+
     cy.findByPlaceholderText(/username/i).type('cypress')
     cy.findByPlaceholderText(/email/i).type('e2e@wongames.com')
     cy.findByPlaceholderText(/^password/i).type('123456')
