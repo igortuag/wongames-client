eb57786ee17e476073c4653433b7bdd4c668111d
Tuag <igortuag@gmail.com>
2022-02-20 07:48:42 -0300

    add intercept method

--- cypress/e2e/fogot-passsword.ts
+++ cypress/e2e/fogot-passsword.ts
@@ -3,5 +3,13 @@
 describe('Forgot Password', () => {
   it.skip('should fill the input and receive a success message', () => {
 
+    cy.intercept('POST', '**/auth/forgot-password', res => {
+      res.reply({
+        status: 200,
+        body: { ok: true}
+      })
+
+      expect(res.body.email).to.eq('ci@wongames.com')
+    })
   });
 })
