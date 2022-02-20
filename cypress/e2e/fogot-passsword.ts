cbf79cbbfda00042cbb027007d569ee8882a2aa1
Tuag <igortuag@gmail.com>
2022-02-20 07:49:59 -0300

    access the forgot page, fill the email and click on button send

--- cypress/e2e/fogot-passsword.ts
+++ cypress/e2e/fogot-passsword.ts
@@ -6,10 +6,15 @@ describe('Forgot Password', () => {
     cy.intercept('POST', '**/auth/forgot-password', res => {
       res.reply({
         status: 200,
-        body: { ok: true}
+        body: { ok: true }
       })
 
       expect(res.body.email).to.eq('ci@wongames.com')
     })
+
+    cy.visit('/forgot-password')
+
+    cy.findAllByPlaceholderText(/email/i).type('ci@wongames.com')
+    cy.findByRole('button', { name: /send email/i }).click()
   });
 })
