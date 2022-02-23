1dbdb4b57ac85e650d92edba296d0898d60ed5c7
Tuag <igortuag@gmail.com>
2022-02-23 06:55:07 -0300

    add tow games to the cart

--- cypress/e2e/cart.ts
+++ cypress/e2e/cart.ts
@@ -4,5 +4,13 @@
 describe('Cart', () => {
   it('should add and remove items from cart', () => {
     cy.visit('/')
+
+    cy.getByDataCy('game-card').eq(1).within(() => {
+      cy.findByRole('button', { name: /Add to cart/i }).click()
+    })
+
+    cy.getByDataCy('game-card').eq(2).within(() => {
+      cy.findByRole('button', { name: /Add to cart/i }).click()
+    })
   })
 })
