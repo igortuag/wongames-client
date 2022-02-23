dfe74700112cb9705d74cb5d1d0a16b63e52062e
Tuag <igortuag@gmail.com>
2022-02-23 06:55:38 -0300

    add more games to cart

--- cypress/e2e/cart.ts
+++ cypress/e2e/cart.ts
@@ -5,6 +5,10 @@ describe('Cart', () => {
   it('should add and remove items from cart', () => {
     cy.visit('/')
 
+    cy.getByDataCy('game-card').eq(0).within(() => {
+      cy.findByRole('button', { name: /Add to cart/i }).click()
+    })
+
     cy.getByDataCy('game-card').eq(1).within(() => {
       cy.findByRole('button', { name: /Add to cart/i }).click()
     })
@@ -12,5 +16,7 @@ describe('Cart', () => {
     cy.getByDataCy('game-card').eq(2).within(() => {
       cy.findByRole('button', { name: /Add to cart/i }).click()
     })
+
+
   })
 })
