00c973fac756a0ac149892fcefdb52e0748880b1
Tuag <igortuag@gmail.com>
2022-02-24 06:38:06 -0300

    use new commands

--- cypress/e2e/cart.ts
+++ cypress/e2e/cart.ts
@@ -5,17 +5,9 @@ describe('Cart', () => {
   it('should add and remove items from cart', () => {
     cy.visit('/')
 
-    cy.getByDataCy('game-card').eq(0).within(() => {
-      cy.findByRole('button', { name: /Add to cart/i }).click()
-    })
-
-    cy.getByDataCy('game-card').eq(1).within(() => {
-      cy.findByRole('button', { name: /Add to cart/i }).click()
-    })
-
-    cy.getByDataCy('game-card').eq(2).within(() => {
-      cy.findByRole('button', { name: /Add to cart/i }).click()
-    })
+    cy.addToCartByIndex(0)
+    cy.addToCartByIndex(1)
+    cy.addToCartByIndex(2)
 
     cy.findAllByLabelText(/cart items/i)
       .first()
@@ -30,17 +22,9 @@ describe('Cart', () => {
       .first()
       .click()
 
-    cy.getByDataCy('game-card').eq(0).within(() => {
-      cy.findByRole('button', { name: /remove from cart/i }).click()
-    })
-
-    cy.getByDataCy('game-card').eq(1).within(() => {
-      cy.findByRole('button', { name: /remove from cart/i }).click()
-    })
-
-    cy.getByDataCy('game-card').eq(2).within(() => {
-      cy.findByRole('button', { name: /remove from cart/i }).click()
-    })
+    cy.removeFromCartByIndex(0)
+    cy.removeFromCartByIndex(1)
+    cy.removeFromCartByIndex(2)
 
     cy.findAllByLabelText(/cart items/i).should('not.exist')
 
