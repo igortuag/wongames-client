c160efb0b9815dfe1fe35e61b1b7eb19251514af
Tuag <igortuag@gmail.com>
2022-02-26 07:48:39 -0300

    add strucuture to test checkout

---
+++ cypress/e2e/checkout.ts
@@ -0,0 +1,16 @@
+/// <reference path="../support/index.d.ts" />
+
+import { createUser, User } from '../support/generate'
+
+describe('Checkout', () => {
+  let user: User
+  describe('Free Games', () => {
+    beforeEach(() => {
+      user = createUser()
+    })
+  })
+
+  describe('Paid Games', () => {
+
+  })
+})
